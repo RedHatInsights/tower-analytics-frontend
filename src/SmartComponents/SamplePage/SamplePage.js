28ae79ebf361698f0312ed69bf4fc8b8743520c1
Ben Thomasson <bthomass@redhat.com>
2019-03-19 13:45:38 -0400

    Add a simple bar chart

--- src/SmartComponents/SamplePage/SamplePage.js
+++ src/SmartComponents/SamplePage/SamplePage.js
@@ -1,13 +1,14 @@
+/* eslint-disable */
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
-import asyncComponent from '../../Utilities/asyncComponent';
 import './sample-page.scss';
+import BarChart from './BarChart.js';
 
 import { Section, Main, PageHeader, PageHeaderTitle } from '@red-hat-insights/insights-frontend-components';
 
 import { Button } from '@patternfly/react-core';
 
-const SampleComponent = asyncComponent(() => import('../../PresentationalComponents/SampleComponent/sample-component'));
+import SampleComponent from '../../PresentationalComponents/SampleComponent/sample-component';
 // const PageHeader2 = asyncComponent(() => import('../../PresentationalComponents/PageHeader/page-header'));
 // const PageHeaderTitle2 = asyncComponent(() => import('../../PresentationalComponents/PageHeader/page-header-title'));
 
@@ -24,20 +25,10 @@ class SamplePage extends Component {
         return (
             <React.Fragment>
                 <PageHeader>
-                    <PageHeaderTitle title='Sample Insights App'/>
-                    <p> This is page header text </p>
+                    <PageHeaderTitle title='Tower Analytics'/>
                 </PageHeader>
                 <Main>
-                    <h1> Sample Component </h1>
-                    <SampleComponent> Sample Component </SampleComponent>
-                    <h1> Cards </h1>
-                    <h1> Buttons </h1>
-                    <Section type='button-group'>
-                        <Button variant='primary'> PF-Next Primary Button </Button>
-                        <Button variant='secondary'> PF-Next Secondary Button </Button>
-                        <Button variant='tertiary'> PF-Next Tertiary Button </Button>
-                        <Button variant='danger'> PF-Next Danger Button </Button>
-                    </Section>
+								<BarChart width={ 700 } height={ 350 } id='bar-chart-root' />
                 </Main>
             </React.Fragment>
         );
