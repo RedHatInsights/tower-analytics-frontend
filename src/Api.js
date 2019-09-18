6410df999c336603fa72e5095cbddecb0d326dbc
Kia Lam <digitalanime@gmail.com>
2019-09-18 18:05:56 -0400

    Add initial pre-flight check and other small changes.
    - Show context-specific empty state for users who are either unauthorized or have no data.
    - Adjust padding for tabs.
    - Extend section background to 100vh.

--- src/Api.js
+++ src/Api.js
@@ -1,8 +1,9 @@
 /*eslint max-len: ["error", { "ignoreStrings": true }]*/
 const templatesEndpoint = '/api/tower-analytics/template_jobs/';
-const clustersEndpoint = '/api/tower-analytics//clusters/';
+const clustersEndpoint = '/api/tower-analytics/clusters/';
+const preflightEndpoint = '/api/tower-analytics/authorized/';
 
-export const templatesRequest = async (id) => {
+export const templatesRequest = async id => {
     if (!id) {
         return;
     }
@@ -16,4 +17,25 @@ export const clustersRequest = async () => {
     let response = await fetch(clustersEndpoint);
     let data = await response.json();
     return data;
+};
+
+export const preflightRequest = async () => {
+    return fetch(preflightEndpoint).then(handleResponse);
+
+    function handleResponse(response) {
+        return response.json().then(json => {
+            if (response.ok) {
+                return json;
+            }
+
+            if (response.status === 404 || response.status === 401) {
+                return Promise.reject({
+                    status: response.status,
+                    message: json
+                });
+            } else {
+                return Promise.reject(json);
+            }
+        });
+    }
 };
