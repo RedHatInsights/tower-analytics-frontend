5215eb1e6ccf39ac792816395a9bc7e35e09147c
Kia Lam <digitalanime@gmail.com>
2020-02-06 17:28:13 -0500

    Replace loading state with no data component when API return empty data.

--- src/Components/NotificationsList.js
+++ src/Components/NotificationsList.js
@@ -142,7 +142,6 @@ const NotificationsList = ({
             >
                 <PFDataListCell>
                     <NoData />
-
                 </PFDataListCell>
             </PFDataListItem>
         ) }
