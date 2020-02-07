f7e11091734400b7cf904bcddac730b7b1cd6358
Kia Lam <digitalanime@gmail.com>
2020-02-06 20:15:50 -0500

    Add NoData component to Org Stats view.

---
+++ src/Components/NoData.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+    Title,
+    EmptyState,
+    EmptyStateVariant,
+    EmptyStateIcon
+} from '@patternfly/react-core';
+import { CubesIcon } from '@patternfly/react-icons';
+
+const NoData = () => (
+    <EmptyState variant={ EmptyStateVariant.full }>
+        <EmptyStateIcon icon={ CubesIcon } />
+        <Title headingLevel="h5" size="lg">
+            No Data
+        </Title>
+    </EmptyState>
+);
+
+export default NoData;
