3450204761f40fd56fce746b1b7b40ed183490db
James Tanner <tanner.jc@gmail.com>
2020-06-11 15:15:41 -0400

    refactor piechart tooltip to understand the data shape

--- src/Charts/PieChart.js
+++ src/Charts/PieChart.js
@@ -16,10 +16,18 @@ const Wrapper = styled.div`
 class Tooltip {
     constructor(props) {
         this.svg = props.svg;
-        this.draw();
+        // defer drawing until we know the data shape
+        this.drawn = false;
+        // show the success rate or not
+        this.showSuccess = false;
     }
 
-    draw() {
+    draw(d) {
+        // chart1 has success rate data and chart2 does not ...
+        this.showSuccess = (d && d.data && 'success_rate' in d.data) ? true : false;
+        const boundingHeight = (this.showSuccess) ? 70 : 52;
+        const boundingWidth = (this.showSuccess) ? 120 : 108;
+
         this.toolTipBase = d3.select(this.svg + '> svg').append('g');
         this.toolTipBase.attr('id', 'svg-chart-Tooltip.base-' + this.svg.slice(1));
         this.toolTipBase.attr('overflow', 'visible');
@@ -40,8 +48,8 @@ class Tooltip {
         .attr('x', 10)
         .attr('y', -23)
         .attr('rx', 2)
-        .attr('height', 70)
-        .attr('width', 120)
+        .attr('height', boundingHeight)
+        .attr('width', boundingWidth)
         .attr('fill', '#393f44');
         this.orgName = this.toolTipBase
         .append('text')
@@ -59,17 +67,26 @@ class Tooltip {
         .attr('x', 20)
         .attr('y', 16)
         .text('0');
-        this.percentageSuccess = this.toolTipBase
-        .append('text')
-        .attr('fill', 'white')
-        .attr('font-size', 12)
-        .attr('font-weight', 800)
-        .attr('x', 20)
-        .attr('y', 30)
-        .text('0');
+        this.percentageSuccess = null;
+        if (this.showSuccess) {
+            this.percentageSuccess = this.toolTipBase
+            .append('text')
+            .attr('fill', 'white')
+            .attr('font-size', 12)
+            .attr('font-weight', 800)
+            .attr('x', 20)
+            .attr('y', 30)
+            .text('');
+        }
     }
 
   handleMouseOver = d => {
+      // treat draw() as a singleton to avoid a painted window effect
+      if (this.drawn !== true) {
+          this.draw(d);
+          this.drawn = true;
+      }
+
       let perc;
       let percSuccess;
       let orgName;
@@ -110,7 +127,10 @@ class Tooltip {
       const flipped = overflow < (x / chartWidth) * 100;
 
       this.percentageTotal.text('' + perc + '%');
-      this.percentageSuccess.text('(' + percSuccess + '% successful)');
+      if (percSuccess && this.percentageSuccess) {
+          this.percentageSuccess.text('(' + percSuccess + '% successful)');
+      }
+
       this.orgName.text(' ' + orgName);
       this.toolTipBase.attr('transform', 'translate(' + x + ',' + y + ')');
       if (flipped) {
@@ -118,13 +138,17 @@ class Tooltip {
           this.boundingBOx.attr('x', -125);
           this.orgName.attr('x', -112);
           this.percentageTotal.attr('x', -112);
-          this.percentageSuccess.attr('x', -112);
+          if (this.showSuccess) {
+              this.percentageSuccess.attr('x', -112);
+          }
       } else {
           this.toolTipPoint.attr('transform', 'translate(10, -10) rotate(45)');
           this.boundingBOx.attr('x', 10);
           this.orgName.attr('x', 20);
           this.percentageTotal.attr('x', 20);
-          this.percentageSuccess.attr('x', 20);
+          if (this.showSuccess) {
+              this.percentageSuccess.attr('x', 20);
+          }
       }
 
       this.toolTipBase.style('opacity', 1);
