c28fa1cdb346ffc7fba891b02a777579a355e21a
Levente Berky <levente.berky@gmail.com>
2020-07-29 07:57:03 +0200

    Fix: Pagination max display limit is reduced to 25 to match API

--- src/Containers/JobExplorer/JobExplorer.js
+++ src/Containers/JobExplorer/JobExplorer.js
@@ -56,9 +56,9 @@ const TitleWithBadge = styled.div`
 const perPageOptions = [
     { title: '5', value: 5 },
     { title: '10', value: 10 },
+    { title: '15', value: 15 },
     { title: '20', value: 20 },
-    { title: '50', value: 50 },
-    { title: '100', value: 100 }
+    { title: '25', value: 25 }
 ];
 
 const initialQueryParams = {
