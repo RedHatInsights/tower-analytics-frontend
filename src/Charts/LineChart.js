c3bfdfaaa65b07bfd2566c271c715a34f1598595
Levente Berky <levente.berky@gmail.com>
2020-08-06 15:16:59 +0200

    Convert LineChart to use the new wrapper

--- src/Charts/LineChart.js
+++ src/Charts/LineChart.js
@@ -1,85 +1,28 @@
-/*eslint camelcase: ["error", {properties: "never"}]*/
-import React, { Component } from 'react';
-import {
-    withRouter
-} from 'react-router-dom';
-import initializeChart from './BaseChart';
-import PropTypes from 'prop-types';
-import Tooltip from '../Utilities/Tooltip';
-import { Paths } from '../paths';
-import { formatDate } from '../Utilities/helpers';
-import { formatQueryStrings } from '../Utilities/formatQueryStrings';
 import * as d3 from 'd3';
 
-class LineChart extends Component {
-    constructor(props) {
-        super(props);
-        this.init = this.init.bind(this);
-        this.draw = this.draw.bind(this);
-        this.resize = this.resize.bind(this);
-        this.updateCluster = this.updateCluster.bind(this);
-        this.redirectToJobExplorer = this.redirectToJobExplorer.bind(this);
-        this.state = {
-            formattedData: [],
-            timeout: null
-        };
-    }
+export const LineChart = props => {
+    const { addXAxis, addYAxis, svg, data, height, width, lineNames } = props;
+    const colors = d3.scaleOrdinal(props.colors);
 
-    redirectToJobExplorer({ DATE: date }) {
-        const { jobExplorer } = Paths;
-        const formattedDate = formatDate(date);
-        const { clusterId } = this.props;
-        const initialQueryParams = {
-            start_date: formattedDate,
-            end_date: formattedDate,
-            quick_date_range: 'custom',
-            status: [ 'failed', 'successful' ],
-            cluster_id: clusterId
-        };
-        const { strings, stringify } = formatQueryStrings(initialQueryParams);
-        const search = stringify(strings);
-        this.props.history.push({
-            pathname: jobExplorer,
-            search
-        });
-    };
+    const setDomains = () => ({
+        x: d3
+        .scaleTime()
+        .rangeRound([ 0, width ])
+        .domain(d3.extent(data, d => d.xAxis)),
+        y: d3
+        .scaleLinear()
+        .range([ height, 0 ])
+        .domain([ 0, d3.max(data, d => d.yAxis * 1.15) ])
+    });
 
-    resize() {
-        const { timeout } = this.state;
-        clearTimeout(timeout);
-        this.setState({
-            timeout: setTimeout(() => { this.init(); }, 500)
-        });
-    }
+    const { x, y } = setDomains();
+    addYAxis(svg, y);
+    addXAxis(svg, x);
 
-    getTickCount() {
-        const { value } = this.props;
-        if (value > 20) {
-            return (value / 2);
-        } else {
-            return value;
-        }
-    }
-    updateCluster() {
-        this.init();
-    }
-    init() {
-        this.draw();
-    }
-    // Methods
-    draw() {
-    // Clear our chart container element first
-        d3.selectAll('#' + this.props.id + ' > *').remove();
-        const width = this.props.getWidth();
-        const height = this.props.getHeight();
+    // Tooltip
+    const tooltip = props.tooltip({ svg: '#' + props.id });
 
-        function transition(path) {
-            path
-            .transition()
-            .duration(1000)
-            .attrTween('stroke-dasharray', tweenDash);
-        }
-
+    const transition = path => {
         function tweenDash() {
             const l = this.getTotalLength();
             const i = d3.interpolateString('0,' + l, l + ',' + l);
@@ -88,258 +31,77 @@ class LineChart extends Component {
             };
         }
 
-        const x = d3.scaleTime().rangeRound([ 0, width ]);
-        const y = d3.scaleLinear().range([ height, 0 ]);
+        path
+        .transition()
+        .duration(1000)
+        .attrTween('stroke-dasharray', tweenDash);
+    };
 
-        //[success, fail, total]
-        let colors = d3.scaleOrdinal([ '#6EC664', '#A30000', '#06C' ]);
-        const svg = d3
-        .select('#' + this.props.id)
-        .append('svg')
-        .attr('width', width + this.props.margin.left + this.props.margin.right)
-        .attr('height', height + this.props.margin.top + this.props.margin.bottom)
-        .attr('z', 100)
-        .append('g')
-        .attr(
-            'transform',
-            'translate(' +
-          this.props.margin.left +
-          ',' +
-          this.props.margin.top +
-          ')'
-        );
-        // Tooltip
-        const tooltip = new Tooltip({
-            svg: '#' + this.props.id,
-            colors
-        });
-        const { data: unformattedData, value } = this.props;
-        const parseTime = d3.timeParse('%Y-%m-%d');
+    const vertical = svg
+    .append('path')
+    .attr('class', 'mouse-line')
+    .style('stroke', 'black')
+    .style('stroke-width', '3px')
+    .style('stroke-dasharray', ('3, 3'))
+    .style('opacity', '0');
 
-        const data = unformattedData.reduce((formatted, { created, successful, failed }) => {
-            let DATE = parseTime(created) || new Date();
-            let RAN = +successful || 0;
-            let FAIL = +failed || 0;
-            let TOTAL = +successful + failed || 0;
-            return formatted.concat({ DATE, RAN, FAIL, TOTAL });
-        }, []);
-        // Scale the range of the data
-        x.domain(
-            d3.extent(data, function(d) {
-                return d.DATE;
-            })
-        );
-        y.domain([
-            0,
-            d3.max(data, function(d) {
-                return d.TOTAL * 1.15 || 8;
-            })
-        ]);
+    const handleMouseOver = d => {
+        tooltip.handleMouseOver(d);
+        vertical.transition().style('opacity', '1');
+    };
 
-        const successLine = d3
-        .line()
+    const handleMouseMove = function(d) {
+        tooltip.handleMouseOver(d);
+        let intersectX = this.cx.baseVal.value;
+        vertical.attr('d', `M${intersectX},${height} ${intersectX}, 0`);
+    };
+
+    const handleMouseOut = () => {
+        tooltip.handleMouseOut();
+        vertical.transition().style('opacity', 0);
+    };
+
+    const setLine = (yAxisName, { x, y }) =>
+        d3.line()
         .curve(d3.curveMonotoneX)
-        .x(function(d) {
-            return x(d.DATE);
-        })
-        .y(function(d) {
-            return y(d.RAN);
-        });
+        .x(d => x(d.xAxis))
+        .y(d => y(d[yAxisName]));
 
-        const failLine = d3
-        .line()
-        .defined(d => !isNaN(d.FAIL))
-        .curve(d3.curveMonotoneX)
-        .x(function(d) {
-            return x(d.DATE);
-        })
-        .y(function(d) {
-            return y(d.FAIL);
-        });
-        // Add the Y Axis
-        svg
-        .append('g')
-        .attr('class', 'y-axis')
-        .call(
-            d3
-            .axisLeft(y)
-            .ticks(10)
-            .tickSize(-width)
-        )
-        .selectAll('line')
-        .attr('stroke', '#d7d7d7');
-        svg.selectAll('.y-axis .tick text').attr('x', -5);
+    const lines = lineNames.map((item, i) => ({
+        path: setLine(item, { x, y }),
+        color: colors(i),
+        name: item
+    }));
 
-        // text label for the y axis
-        svg
-        .append('text')
-        .attr('transform', 'rotate(-90)')
-        .attr('y', 0 - this.props.margin.left)
-        .attr('x', 0 - height / 2)
-        .attr('dy', '1em')
-        .style('text-anchor', 'middle')
-        .text('Job runs');
-        // Add the X Axis
-        let ticks;
-        const maxTicks = Math.round(data.length / (value / 2));
-        ticks = data.map(d => d.DATE);
-        if (value === 31) {
-            ticks = data.map((d, i) =>
-                i % maxTicks === 0 ? d.DATE : undefined).filter(item => item);
-        }
-
-        svg
-        .append('g')
-        .attr('class', 'x-axis')
-        .attr('transform', 'translate(0,' + height + ')')
-        .call(
-            d3
-            .axisBottom(x)
-            .tickValues(ticks)
-            .tickSize(-height)
-            .tickFormat(d3.timeFormat('%-m/%-d')) // "1/19"
-        ) // "Jan-01"
-        .selectAll('line')
-        .attr('stroke', '#d7d7d7');
-        svg.selectAll('.x-axis .tick text').attr('y', 10);
-
-        // text label for the x axis
-        svg
-        .append('text')
-        .attr(
-            'transform',
-            'translate(' +
-                width / 2 +
-                ' ,' +
-                (height + this.props.margin.top + 20) +
-                ')'
-        )
-        .style('text-anchor', 'middle')
-        .text('Date');
-        const vertical = svg
-        .append('path')
-        .attr('class', 'mouse-line')
-        .style('stroke', 'black')
-        .style('stroke-width', '3px')
-        .style('stroke-dasharray', ('3, 3'))
-        .style('opacity', '0');
-
-        const handleMouseOver = function(d) {
-            tooltip.handleMouseOver(d);
-            // show vertical line
-            vertical.transition().style('opacity', '1');
-        };
-
-        const handleMouseMove = function() {
-            let intersectX = this.cx.baseVal.value;
-            vertical
-            .attr('d', function () {
-                let d = 'M' + intersectX + ',' + height;
-                d += ' ' + intersectX + ',' + 0;
-                return d;
-            });
-        };
-
-        const handleMouseOut = function() {
-            // hide tooltip
-            tooltip.handleMouseOut();
-            // hide vertical line
-            vertical.transition().style('opacity', 0);
-        };
-
-        // Add the successLine path.
+    lines.forEach(line => {
+        // Add lines
         svg
         .append('path')
         .data([ data ])
         .attr('class', 'line')
         .style('fill', 'none')
-        .style('stroke', () => colors(1))
+        .style('stroke', line.color)
         .attr('stroke-width', 2)
-        .attr('d', successLine)
+        .attr('d', line.path)
         .call(transition);
 
-        // Add the failLine path.
-        svg
-        .append('path')
-        .data([ data ])
-        .attr('class', 'line')
-        .style('fill', 'none')
-        .style('stroke', () => colors(0))
-        .attr('stroke-width', 2)
-        .attr('d', failLine)
-        .call(transition);
+        // Add circles
         svg
         .selectAll('dot')
         .data(data)
         .enter()
         .append('circle')
         .attr('r', 3)
-        .style('stroke', () => colors(1))
-        .style('fill', () => colors(1))
-        .attr('cx', function(d) {
-            return x(d.DATE);
-        })
-        .attr('cy', function(d) {
-            return y(d.RAN);
-        })
+        .style('stroke', line.color)
+        .style('fill', line.color)
+        .attr('cx', d => x(d.xAxis))
+        .attr('cy', d => y(d[line.name]))
 
         .on('mouseover', handleMouseOver)
         .on('mousemove', handleMouseMove)
         .on('mouseout', handleMouseOut)
-        .on('click', this.redirectToJobExplorer);
-        // create our failLine circles
-        svg
-        .selectAll('dot')
-        .data(data)
-        .enter()
-        .append('circle')
-        .attr('r', 3)
-        .style('stroke', () => colors(0))
-        .style('fill', () => colors(0))
-        .attr('cx', function(d) {
-            return x(d.DATE);
-        })
-        .attr('cy', function(d) {
-            return y(d.FAIL);
-        })
-        .on('mouseover', handleMouseOver)
-        .on('mousemove', handleMouseMove)
-        .on('mouseout', handleMouseOut)
-        .on('click', this.redirectToJobExplorer);
-    }
-
-    componentDidMount() {
-        this.updateCluster();
-        // Call the resize function whenever a resize event occurs
-        window.addEventListener('resize', this.resize);
-    }
-
-    componentDidUpdate(prevProps) {
-        if (prevProps.value !== this.props.value) {
-            this.updateCluster();
-        }
-    }
-
-    componentWillUnmount() {
-        const { timeout } = this.state;
-        clearTimeout(timeout);
-        window.removeEventListener('resize', this.resize);
-    }
-
-    render() {
-        return <div id={ this.props.id } />;
-    }
-}
-
-LineChart.propTypes = {
-    id: PropTypes.string,
-    data: PropTypes.array,
-    value: PropTypes.number,
-    margin: PropTypes.object,
-    getHeight: PropTypes.func,
-    getWidth: PropTypes.func,
-    history: PropTypes.object,
-    clusterId: PropTypes.number
+        .on('click', ({ xAxis }) => props.onClick(xAxis));
+    });
 };
 
-export default initializeChart(withRouter(LineChart));
+export default LineChart;
