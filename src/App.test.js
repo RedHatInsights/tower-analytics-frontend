3b7a7789304484d3e8639f6c2e1c06b92f4bab47
James Tanner <tanner.jc@gmail.com>
2020-09-08 11:28:23 -0400

    add app test for additional coverage

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,63 @@
 
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import App from './App.js';
+import { Router } from 'react-router-dom';
 
-describe('Charts/App', () => {
+import configureStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
+
+import packageJson from '../package.json';
+
+describe('App', () => {
     it('App loads', () => {
         expect(App).toBeTruthy();
     });
     it('should render successfully', () => {
         shallow(<App />);
+    });
+    it('should return a div with a version attribute', () => {
+
+        // the insights object
+        const mockInsights = {
+            chrome: {
+                on() {},
+                init() {},
+                identifyApp() {},
+                auth: {
+                    getUser: () => {
+                        return 'bob';
+                    }
+                }
+            }
+        };
+
+        const history = {
+            listen() {
+                return { unlisten() {} };
+            },
+            replace() {},
+            push() {},
+            length: 1,
+            action: 'PUSH',
+            location: {
+                pathname: '/somewhere',
+                search: '',
+                hash: '#howdy'
+            }
+        };
+
+        const mockStore = configureStore();
+        const store = mockStore({});
+        global.insights = mockInsights;
+        let wrapper = mount(
+            <Provider store={ store } >
+                <Router history={ history }>
+                    <App />
+                </Router>
+            </Provider>
+        );
+
+        let componentVersion = wrapper.find('#automation-analytics-application').props().version;
+        expect(componentVersion).toBe(packageJson.version);
     });
 });
