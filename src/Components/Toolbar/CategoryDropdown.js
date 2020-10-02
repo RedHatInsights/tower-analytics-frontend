16a5fe2d9c344a15610ffac8f8a09d125e257435
Levente Berky <levente.berky@gmail.com>
2020-10-02 15:58:15 +0200

    Refactoring: toolbar selector changed to select insread of dropdown

---
+++ src/Components/Toolbar/CategoryDropdown.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import {
+    ToolbarItem,
+    Select,
+    SelectOption
+} from '@patternfly/react-core';
+
+const CategoryDropdown = ({
+    selected,
+    setSelected = () => {},
+    categories = []
+}) => {
+    const [ isExpanded, setIsExpanded ] = useState(false);
+
+    return (
+        <ToolbarItem>
+            <Select
+                isOpen={ isExpanded }
+                variant={ 'single' }
+                aria-label={ 'Categories' }
+                onToggle={ () => setIsExpanded(!isExpanded) }
+                onSelect={ (_event, selection) => {
+                    setSelected(selection);
+                    setIsExpanded(false);
+                } }
+                selections={ selected }
+                placeholderText={ 'Filter by' }
+            >
+                {
+                    categories.map(({ key, name }) => (
+                        <SelectOption key={ key } value={ key }>
+                            { name }
+                        </SelectOption>
+                    ))
+                }
+            </Select>
+        </ToolbarItem>
+    );
+};
+
+CategoryDropdown.propTypes = {
+    categories: PropTypes.array,
+    selected: PropTypes.string,
+    setSelected: PropTypes.func
+};
+
+export default CategoryDropdown;
