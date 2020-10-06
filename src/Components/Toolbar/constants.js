298d6636ee70224c18e782d4b12fba5bdbeb86bf
Levente Berky <levente.berky@gmail.com>
2020-10-06 10:05:02 +0200

    Filter toolbar constant move outside the toolbar for mantainabilty

---
+++ src/Components/Toolbar/constants.js
@@ -0,0 +1,37 @@
+export const optionsForCategories = {
+    status: {
+        single: false,
+        name: 'Status',
+        placeholder: 'Filter by job status'
+    },
+    quickDateRange: {
+        single: true,
+        name: 'Date',
+        placeholder: 'Filter by date'
+    },
+    jobType: {
+        single: false,
+        name: 'Job',
+        placeholder: 'Filter by job type'
+    },
+    orgId: {
+        single: false,
+        name: 'Organization',
+        placeholder: 'Filter by organization'
+    },
+    clusterId: {
+        single: false,
+        name: 'Cluster',
+        placeholder: 'Filter by cluster'
+    },
+    templateId: {
+        single: false,
+        name: 'Template',
+        placeholder: 'Filter by template'
+    },
+    sortBy: {
+        single: true,
+        name: 'Sort by',
+        placeholder: 'Sort by attribute'
+    }
+};
