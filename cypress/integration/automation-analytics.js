1ad1feac704c97cbd43906adc9679ed32b6899bc
James Tanner <tanner.jc@gmail.com>
2020-12-02 12:47:32 -0500

    tests for org stats page

--- cypress/integration/automation-analytics.js
+++ cypress/integration/automation-analytics.js
@@ -18,6 +18,7 @@ import {
     getBaseUrl,
     getUsername,
     getPassword,
+    getUniqueRandomNumbers,
     hasInnerHrefs,
     hasInnerButtons,
     waitDuration
@@ -52,6 +53,63 @@ async function fuzzClustersPage() {
         aalink.click();
         cy.wait(waitDuration);
     }
+
+}
+
+async function fuzzOrgStatsPage() {
+
+    // select 3 random bars to view events for ...
+    let selectedIDs = getUniqueRandomNumbers(10, 3);
+    cy.log('SELECTEDIDS', selectedIDs);
+    selectedIDs.forEach((xid) => {
+        cy.log(xid);
+
+        // click on the rectangle ...
+        cy.get(appid).find('rect').eq(xid).click({force: true});
+        cy.wait(waitDuration);
+
+        // go back to the stats page ...
+        const statslink = cy.get('a[href="' + getBaseUrl() + '/ansible/automation-analytics/organization-statistics"]')
+        statslink.click();
+        cy.wait(waitDuration);
+    })
+    
+    // toggle each org off/on
+    cy.get(appid).find('span[class="pf-c-switch__toggle"]').each((toggle, ix) => {
+        //toggle.click().wait(waitDuration);
+        //toggle.click().wait(waitDuration);
+        cy.get(appid).find('span[class="pf-c-switch__toggle"]').eq(ix).click();
+        cy.wait(waitDuration);
+        cy.get(appid).find('span[class="pf-c-switch__toggle"]').eq(ix).click();
+        cy.wait(waitDuration);
+
+    })
+
+    // flip through each org grouping ...
+    let orgGroups = ["All Orgs", "Bottom 5 Orgs", "Top 5 Orgs"];
+    orgGroups.forEach((group) => {
+        cy.get(appid).find('select[name="sortOrder"]').eq(0).select(group).wait(waitDuration);
+        cy.wait(waitDuration);
+    })    
+
+    // flip through each time grouping ...
+    let timeGroups = ["Past Week", "Past 2 Weeks", "Past Month"];
+    timeGroups.forEach((group) => {
+        cy.get(appid).find('select[name="timeframe"]').eq(0).select(group).wait(waitDuration);
+        cy.wait(waitDuration);
+    })
+
+    // hover over each donut1 pie slice ...
+    cy.get(appid).find('#d3-donut-1-chart-root > svg > g > path').each((slice, ix) => {
+        cy.get(appid).find('#d3-donut-1-chart-root > svg > g > path').eq(ix).trigger('mouseover', {force: true});
+        cy.wait(waitDuration);
+    })
+
+    // hover over each donut2 pie slice ...
+    cy.get(appid).find('#d3-donut-2-chart-root > svg > g > path').each((slice, ix) => {
+        cy.get(appid).find('#d3-donut-2-chart-root > svg > g > path').eq(ix).trigger('mouseover', {force: true});
+        cy.wait(waitDuration);
+    })
 
 }
 
@@ -104,7 +162,7 @@ beforeEach(() => {
 
 describe('automation analytics smoketests', () => {
 
-    xit('has all the AA navigation items', () => {
+    it('has all the AA navigation items', () => {
         cy.visit(getBaseUrl());
         const aalink = cy.get('a[href="/ansible/automation-analytics"]').first();
         aalink.click();
@@ -116,7 +174,7 @@ describe('automation analytics smoketests', () => {
         navlis.should('have.length', 5)
     })
 
-    xit('can open each page without breaking the UI', () => {
+    it('can open each page without breaking the UI', () => {
         cy.visit(getBaseUrl());
         const aalink = cy.get('a[href="/ansible/automation-analytics"]').first();
         aalink.click();
@@ -124,8 +182,6 @@ describe('automation analytics smoketests', () => {
 
         cy.get('li[ouiaid="automation-analytics"] > section > ul > li > a').each((href, hid) => {
             cy.log('href', hid, href[0].pathname);
-            navurls.push(href[0].pathname);
-            console.log(navurls);
 
             cy.visit(getBaseUrl() + href[0].pathname);
             cy.wait(waitDuration);
@@ -138,7 +194,7 @@ describe('automation analytics smoketests', () => {
 
     })
 
-    xit('can interact with the clusters page without breaking the UI', () => {
+    it('can interact with the clusters page without breaking the UI', () => {
         cy.visit(getBaseUrl());
         const aalink = cy.get('a[href="/ansible/automation-analytics"]').first();
         aalink.click();
@@ -146,8 +202,6 @@ describe('automation analytics smoketests', () => {
 
         cy.get('li[ouiaid="automation-analytics"] > section > ul > li > a').first().each((href, hid) => {
             cy.log('href', hid, href[0].pathname);
-            navurls.push(href[0].pathname);
-            console.log(navurls);
 
             cy.visit(getBaseUrl() + href[0].pathname);
             cy.wait(waitDuration);
@@ -162,6 +216,25 @@ describe('automation analytics smoketests', () => {
 
     })
 
+    it('can interact with the org stats page without breaking the UI', () => {
+        cy.visit(getBaseUrl());
+        const aalink = cy.get('a[href="/ansible/automation-analytics"]').first();
+        aalink.click();
+        cy.wait(waitDuration);
+
+        cy.get('li[ouiaid="automation-analytics"] > section > ul > li > a').eq(1).each((href, hid) => {
+            cy.log('href', hid, href[0].pathname);
+
+            cy.visit(getBaseUrl() + href[0].pathname);
+            cy.wait(waitDuration);
+            clearFeatureDialogs();
+
+            fuzzOrgStatsPage();
+
+        });
+
+    })
+
     it('can interact with the notifications page without breaking the UI', () => {
         cy.visit(getBaseUrl());
         const aalink = cy.get('a[href="/ansible/automation-analytics"]').first();
@@ -170,8 +243,6 @@ describe('automation analytics smoketests', () => {
 
         cy.get('li[ouiaid="automation-analytics"] > section > ul > li > a').eq(3).each((href, hid) => {
             cy.log('href', hid, href[0].pathname);
-            navurls.push(href[0].pathname);
-            console.log(navurls);
 
             cy.visit(getBaseUrl() + href[0].pathname);
             cy.wait(waitDuration);
