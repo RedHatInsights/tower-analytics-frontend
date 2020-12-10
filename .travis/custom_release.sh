be332f17becef5748b920e3f71bc51b796c2a686
Ben Thomasson <bthomass@redhat.com>
2020-12-10 12:08:34 -0500

    Push to all the betas from devel

--- .travis/custom_release.sh
+++ .travis/custom_release.sh
@@ -2,12 +2,14 @@
 set -e
 set -x
 
-# If current dev branch is devel, push to build repo ci-beta
+# If current dev branch is devel, push to build repo ci-beta, qa-beta, and prod-beta
 if [ "${TRAVIS_BRANCH}" = "devel" ]; then
     .travis/release.sh "ci-beta"
+    .travis/release.sh "qa-beta"
+    .travis/release.sh "prod-beta"
 fi
 
 # If current dev branch is deployment branch, push to build repo
-if [[ "${TRAVIS_BRANCH}" = "ci-stable"  || "${TRAVIS_BRANCH}" = "qa-beta" || "${TRAVIS_BRANCH}" = "qa-stable" || "${TRAVIS_BRANCH}" = "prod-beta" || "${TRAVIS_BRANCH}" = "prod-stable" || "${TRAVIS_BRANCH}" = "qaprodauth-stable" || "${TRAVIS_BRANCH}" = "qaprodauth-beta" ]]; then
+if [[ "${TRAVIS_BRANCH}" = "ci-stable"  || "${TRAVIS_BRANCH}" = "qa-stable" || "${TRAVIS_BRANCH}" = "prod-stable" || "${TRAVIS_BRANCH}" = "qaprodauth-stable" || "${TRAVIS_BRANCH}" = "qaprodauth-beta" ]]; then
     .travis/release.sh "${TRAVIS_BRANCH}"
 fi
