c38a70312dd2a968ba738751d0eb918f23aeec6b
Levente Berky <levente.berky@gmail.com>
2021-02-17 13:14:12 +0100

    Updated tests for the GroupedBarChart tooltips

--- src/Charts/GroupedBarChart/GroupedBarChart.test.js
+++ src/Charts/GroupedBarChart/GroupedBarChart.test.js
@@ -1,11 +1,21 @@
 import { shallow } from 'enzyme';
-import GroupedBarChart from './GroupedBarChart.js';
+import { GroupedBarChart, HostsTooltip, OrgsTooltip } from './index';
 
 describe('Charts/GroupedBarChart', () => {
     it('GroupedBarChart loads', () => {
         expect(GroupedBarChart).toBeTruthy();
     });
-    it('should render successfully', () => {
-        shallow(<GroupedBarChart />);
+
+    it('should render successfully with Orgs Tooltip', () => {
+        shallow(<GroupedBarChart
+            TooltipClass={ OrgsTooltip }
+        />);
     });
+
+    it('should render successfully with Hosts Tooltip', () => {
+        shallow(<GroupedBarChart
+            TooltipClass={ HostsTooltip }
+        />);
+    });
+
 });
