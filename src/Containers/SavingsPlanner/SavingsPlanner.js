080fd863acc251f5980ee5d421f444833cea54aa
Kia Lam <digitalanime@gmail.com>
2021-04-21 13:25:16 -0400

    A few changes:
    - Break out card into its own component.
    - Add placeholder kebab options.
    - Add error and loading states.

--- src/Containers/SavingsPlanner/SavingsPlanner.js
+++ src/Containers/SavingsPlanner/SavingsPlanner.js
@@ -1,6 +1,5 @@
 /* eslint-disable */
-import React, { useState, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import {
@@ -8,10 +7,10 @@ import {
     readPlanOptions,
     readPlans
 } from '../../Api';
-
-import { Paths } from '../../paths';
-
-import { formatDateTime } from '../../Utilities/helpers';
+import ApiErrorState from '../../Components/ApiErrorState';
+import LoadingState from '../../Components/LoadingState';
+import PlanCard from './PlanCard';
+import useApi from '../../Utilities/useApi';
 
 import {
     Main,
@@ -19,49 +18,11 @@ import {
     PageHeaderTitle
 } from '@redhat-cloud-services/frontend-components';
 
-import useApi from '../../Utilities/useApi';
-
 import {
-    Card,
-    CardHeader,
-    CardHeaderMain,
-    CardActions,
-    CardTitle,
-    CardBody,
-    CardFooter,
-    Checkbox,
-    Dropdown,
-    KebabToggle,
-    Label,
     Gallery
 } from '@patternfly/react-core';
 
-import { ChartBarIcon, CheckCircleIcon, ExclamationCircleIcon } from '@patternfly/react-icons';
-import styled from 'styled-components';
-import { stringify } from 'query-string';
-
-
-const CardLabel = styled.span`
-  margin-right: 5px;
-`;
-
 const SavingsPlanner = () => {
-    let history = useHistory();
-    
-    const redirectToJobExplorer = templateId => {
-        const { jobExplorer } = Paths;
-        const initialQueryParams = {
-            quick_date_range: 'last_30_days',
-            status: ['failed', 'successful'],
-            template_id: [ templateId ]
-        };
-        const search = stringify(initialQueryParams, { arrayFormat: 'bracket' });
-        history.push({
-            pathname: jobExplorer,
-            search
-        });
-    }
-
     const [
         {
             isLoading,
@@ -82,80 +43,35 @@ const SavingsPlanner = () => {
         fetchEndpoints();
     }, []);
 
-    const showTemplate = id => {
-        if (!id) {
-            return;
-        };
-        if (options.isSuccess) {
-            const selectedTemplate = options.data.templates.filter(t => t.id === id);
-            return(
-                <a onClick={() => redirectToJobExplorer(selectedTemplate[0].id)}>{selectedTemplate[0].name}</a>
-            );
-        }
-    }
-
     return (
         <React.Fragment>
             <PageHeader>
                 <PageHeaderTitle title={'Savings Planner'} />
             </PageHeader>
-                <Main>
-                <Gallery hasGutter>
-                    { isSuccess && data.map(i => (
-                        <Card isHoverable isCompact key={i.id}>
-                            <CardHeader>
-                                <CardHeaderMain>
-                                    <CardTitle>{i.name}</CardTitle>
-                                </CardHeaderMain>
-                                <CardActions>
-                                    <Dropdown
-                                        onSelect={() => { }}
-                                        toggle={<KebabToggle onToggle={() => { }} />}
-                                        isOpen={false}
-                                        isPlain
-                                        dropdownItems={[]}
-                                        position={'right'}
-                                    />
-                                    <Checkbox
-                                        isChecked={false}
-                                        onChange={() => { }}
-                                        aria-label="card checkbox example"
-                                        id="check-1"
-                                        name="check1"
-                                    />
-                                </CardActions>
-                            </CardHeader>
-                            <CardBody>
-                                {i.description ? (<p>{i.description}</p>) : null}
-                                <div>
-                                    <CardLabel>Frequency</CardLabel> {i.frequency_period ? i.frequency_period : (<em>None</em>)}
-                                </div>
-                                <div>
-                                    <CardLabel>Template</CardLabel> {i.template_id ? showTemplate(i.template_id) : (<em>None</em>)}
-                                </div>
-                                <div>
-                                    <CardLabel>Automation status</CardLabel>
-                                        { i.automation_status.status === 'successful' ? (
-                                            <Label variant="outline" color="green" icon={<CheckCircleIcon />}>
-                                                Running
-                                            </Label>
-                                        ) : (
-                                            <Label variant="outline" color="red" icon={<ExclamationCircleIcon />}>
-                                                Not Running
-                                            </Label>
-                                        )}
-                                </div>
-                                <div>
-                                    <CardLabel>Last updated</CardLabel> {formatDateTime(i.modified)}
-                                </div>
-                            </CardBody>
-                            <CardFooter>
-                                <Label>{i.category}</Label>
-                            </CardFooter>
-                        </Card>
-                    ))}
-                </Gallery>
-                </Main>
+                {error && (
+                    <Main>
+                        <ApiErrorState message={error.error} />
+                    </Main>
+                )}
+                {isLoading && (
+                    <Main>
+                        <LoadingState />
+                    </Main>
+                )}
+                {isSuccess && (
+                    <Main>
+                        <Gallery hasGutter>
+                            {options.isSuccess && data.map(datum => (
+                                <PlanCard
+                                    key={datum.id}
+                                    isSuccess={options.isSuccess}
+                                    templates={options.data.templates}
+                                    {...datum}
+                                />
+                            ))}
+                        </Gallery>
+                    </Main>
+                )}
         </React.Fragment>
     );
 };
