7302a6cb66de664af8ac0053cfb776295b1a920a
Kia Lam <digitalanime@gmail.com>
2021-04-22 12:07:23 -0400

    WIP hook up pagination.

--- src/Containers/SavingsPlanner/SavingsPlanner.js
+++ src/Containers/SavingsPlanner/SavingsPlanner.js
@@ -1,15 +1,16 @@
 /* eslint-disable */
-import React, { useEffect } from 'react';
-import PropTypes from 'prop-types';
+import React, { useEffect, useState } from 'react';
 
 import {
     preflightRequest,
     readPlanOptions,
     readPlans
 } from '../../Api';
+import FilterableToolbar from '../../Components/Toolbar/';
 import ApiErrorState from '../../Components/ApiErrorState';
 import LoadingState from '../../Components/LoadingState';
 import PlanCard from './PlanCard';
+import { useQueryParams } from '../../Utilities/useQueryParams';
 import useApi from '../../Utilities/useApi';
 
 import {
@@ -19,34 +20,94 @@ import {
 } from '@redhat-cloud-services/frontend-components';
 
 import {
-    Gallery
+    Gallery,
+    Pagination,
+    PaginationVariant
 } from '@patternfly/react-core';
 
+const perPageOptions = [
+    { title: '5', value: 5 },
+    { title: '10', value: 10 },
+    { title: '15', value: 15 },
+    { title: '20', value: 20 },
+    { title: '25', value: 25 }
+];
+
+const qp = {
+    limit: 5
+};
+
+
 const SavingsPlanner = () => {
+    const {
+        queryParams,
+        setLimit,
+        setOffset,
+        setFromToolbar,
+        dispatch
+    } = useQueryParams(qp);
     const [
         {
             isLoading,
             isSuccess,
             error,
-            data: { items: data = [] }
+            data: { meta = {}, items: data = [] }
         },
         setData
-    ] = useApi({ items: [] });
+    ] = useApi({ meta: {}, items: [] });
     const [ options, setOptions ] = useApi({});
+    const [ currPage, setCurrPage ] = useState(1);
+
+    const returnOffsetVal = page => (page - 1) * queryParams.limit;
+
+    const handleSetPage = page => {
+        const nextOffset = returnOffsetVal(page);
+        setOffset(nextOffset);
+        setCurrPage(page);
+    };
+
+    const handlePerPageSelect = (perPage, page) => {
+        setLimit(perPage);
+        const nextOffset = returnOffsetVal(page);
+        setOffset(nextOffset);
+        setCurrPage(page);
+    };
 
     useEffect(() => {
         const fetchEndpoints = () => {
-            setData(readPlans({ params: { limit: 10 } }));
+            setData(readPlans({ params: queryParams }));
             setOptions(readPlanOptions());
         };
 
         fetchEndpoints();
-    }, []);
+    }, [ queryParams ]);
 
     return (
         <React.Fragment>
             <PageHeader>
                 <PageHeaderTitle title={'Savings Planner'} />
+                <FilterableToolbar
+                    categories={options.data}
+                    filters={queryParams}
+                    setFilters={setFromToolbar}
+                    pagination={
+                        <Pagination
+                            itemCount={meta && meta.total_count ? meta.total_count : 0}
+                            widgetId="pagination-options-menu-top"
+                            perPageOptions={perPageOptions}
+                            perPage={queryParams.limit}
+                            page={currPage}
+                            variant={PaginationVariant.top}
+                            onPerPageSelect={(_event, perPage, page) => {
+                                handlePerPageSelect(perPage, page);
+                            }}
+                            onSetPage={(_event, pageNumber) => {
+                                handleSetPage(pageNumber);
+                            }}
+                            isCompact
+                        />
+                    }
+                />
             </PageHeader>
                 {error && (
                     <Main>
@@ -74,9 +135,6 @@ const SavingsPlanner = () => {
                 )}
         </React.Fragment>
     );
-};
-
-SavingsPlanner.propTypes = {
 };
 
 export default SavingsPlanner;
