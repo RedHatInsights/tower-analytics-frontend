97d9ffe68c81366045fecc68c92d1a7f70b857d7
Martin Marosi <marvusm.mmi@gmail.com>
2021-05-06 15:27:29 +0200

    Update proxy config.

--- profiles/local-frontend.js
+++ profiles/local-frontend.js
@@ -1,11 +1,12 @@
-/*global module*/
 const APP_ID = 'automation-analytics';
 const FRONTEND_PORT = 8002;
 const routes = {};
 
-routes[`/beta/apps/${APP_ID}`]       = { host: `https://localhost:${FRONTEND_PORT}` };
-routes[`/apps/${APP_ID}`]            = { host: `https://localhost:${FRONTEND_PORT}` };
+routes[`/beta/apps/${APP_ID}`] = { host: `https://localhost:${FRONTEND_PORT}` };
+routes[`/apps/${APP_ID}`] = { host: `https://localhost:${FRONTEND_PORT}` };
 routes[`/ansible/insights`] = { host: `https://localhost:${FRONTEND_PORT}` };
-routes[`/beta/ansible/insights`] = { host: `https://localhost:${FRONTEND_PORT}` };
+routes[`/beta/ansible/insights`] = {
+  host: `https://localhost:${FRONTEND_PORT}`,
+};
 
-module.exports = { routes };  
+module.exports = { routes };
