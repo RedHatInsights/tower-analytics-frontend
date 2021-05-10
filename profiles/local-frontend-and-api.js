22725b7f25d6d8093e0bf22ab211a5b30d773e7d
Kia Lam <digitalanime@gmail.com>
2021-05-10 13:31:49 -0400

    Update scripts and proxy configs to work with local api setup.

--- profiles/local-frontend-and-api.js
+++ profiles/local-frontend-and-api.js
@@ -1,17 +1,18 @@
 /*global module*/
 
-const SECTION = 'ansible';
 const APP_ID = 'automation-analytics';
 const FRONTEND_PORT = 8002;
-const API_PORT = 8888;
+const API_PORT = 8004;
 const routes = {};
 
 
-routes[`/beta/${SECTION}/${APP_ID}`] = { host: `http://localhost:${FRONTEND_PORT}` };
-routes[`/${SECTION}/${APP_ID}`]      = { host: `http://localhost:${FRONTEND_PORT}` };
-routes[`/beta/apps/${APP_ID}`]       = { host: `http://localhost:${FRONTEND_PORT}` };
-routes[`/apps/${APP_ID}`]            = { host: `http://localhost:${FRONTEND_PORT}` };
+routes[`/beta/apps/${APP_ID}`] = { host: `https://localhost:${FRONTEND_PORT}` };
+routes[`/apps/${APP_ID}`] = { host: `https://localhost:${FRONTEND_PORT}` };
+routes[`/ansible/insights`] = { host: `https://localhost:${FRONTEND_PORT}` };
+routes[`/beta/ansible/insights`] = {
+    host: `https://localhost:${FRONTEND_PORT}`,
+};
 
-routes[`/api/${APP_ID}`] = { host: `http://localhost:${API_PORT}` };
+routes[`/api/tower-analytics`] = { host: `http://localhost:${API_PORT}` };
 
 module.exports = { routes };
