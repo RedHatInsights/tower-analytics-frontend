12c71abaa76a235cfe3f36d523445556fcf4a8f6
Kia Lam <digitalanime@gmail.com>
2021-05-11 16:45:31 -0400

    Add route for local side nav config.

--- profiles/local-frontend-and-api.js
+++ profiles/local-frontend-and-api.js
@@ -5,14 +5,13 @@ const FRONTEND_PORT = 8002;
 const API_PORT = 8004;
 const routes = {};
 
-
 routes[`/beta/apps/${APP_ID}`] = { host: `https://localhost:${FRONTEND_PORT}` };
 routes[`/apps/${APP_ID}`] = { host: `https://localhost:${FRONTEND_PORT}` };
 routes[`/ansible/insights`] = { host: `https://localhost:${FRONTEND_PORT}` };
 routes[`/beta/ansible/insights`] = {
     host: `https://localhost:${FRONTEND_PORT}`,
 };
-
+routes[`/beta/config`] = { host: `http://localhost:8889` };
 routes[`/api/tower-analytics`] = { host: `http://localhost:${API_PORT}` };
 
 module.exports = { routes };
