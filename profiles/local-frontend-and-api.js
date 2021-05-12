c7d21206bdee6308e5b5105c959aeacb4102e7b9
Levente Berky <levente.berky@gmail.com>
2021-05-12 14:24:08 +0200

    Updating commands, removing the unused config
    Merged the main.yml to this repo, so we can use the http server to serve from the repo.
    This would allow not to pull it from outside, reducing the dependencies
    we need for development.

--- profiles/local-frontend-and-api.js
+++ profiles/local-frontend-and-api.js
@@ -1,5 +1,3 @@
-/*global module*/
-
 const APP_ID = 'automation-analytics';
 const FRONTEND_PORT = 8002;
 const API_PORT = 8004;
@@ -9,9 +7,9 @@ routes[`/beta/apps/${APP_ID}`] = { host: `https://localhost:${FRONTEND_PORT}` };
 routes[`/apps/${APP_ID}`] = { host: `https://localhost:${FRONTEND_PORT}` };
 routes[`/ansible/insights`] = { host: `https://localhost:${FRONTEND_PORT}` };
 routes[`/beta/ansible/insights`] = {
-    host: `https://localhost:${FRONTEND_PORT}`,
+  host: `https://localhost:${FRONTEND_PORT}`,
 };
-routes[`/beta/config`] = { host: `http://localhost:8889` };
+routes[`/beta/config/main.yml`] = { host: `http://localhost:8889` };
 routes[`/api/tower-analytics`] = { host: `http://localhost:${API_PORT}` };
 
 module.exports = { routes };
