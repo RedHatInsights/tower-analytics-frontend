543c4b4524898bfa9c5e54e7f979b4312fdc46f4
Kia Lam <digitalanime@gmail.com>
2021-06-05 14:14:06 -0400

    Update failing tests.

--- src/Containers/SavingsPlanner/Shared/Tasks.test.js
+++ src/Containers/SavingsPlanner/Shared/Tasks.test.js
@@ -3,19 +3,19 @@ import { render, fireEvent, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 describe('SavingsPlanner/Shared/Tasks', () => {
-  const setField = jest.fn();
+  const mockDispatch = jest.fn();
   const mockTasks = ['foo', 'bar', 'baz'];
 
   beforeEach(() => {
-    setField.mockReset();
+    mockDispatch.mockReset();
   });
 
   it('can see the shared/Tasks component', () => {
-    render(<Tasks tasks={mockTasks} setField={setField} />);
+    render(<Tasks tasks={mockTasks} dispatch={mockDispatch} />);
   });
 
   it('can add a task by clicking "Add" button', async () => {
-    render(<Tasks tasks={mockTasks} setField={setField} />);
+    render(<Tasks tasks={mockTasks} dispatch={mockDispatch} />);
     userEvent.type(
       screen.getByRole('textbox', {
         name: 'What tasks do you need to accomplish this plan?',
@@ -23,22 +23,22 @@ describe('SavingsPlanner/Shared/Tasks', () => {
       'qux'
     );
     fireEvent.click(screen.getByRole('button', { name: 'Add task' }));
-    expect(setField).toHaveBeenCalledWith('tasks', mockTasks.concat('qux'));
+    expect(mockDispatch).toHaveBeenCalledWith({"type": "SET_TASKS", "value": mockTasks.concat('qux')});
   });
 
   it('can add a task by hitting "enter" key', () => {
-    render(<Tasks tasks={mockTasks} setField={setField} />);
+    render(<Tasks tasks={mockTasks} dispatch={mockDispatch} />);
     const taskInput = screen.getByRole('textbox', {
       name: 'What tasks do you need to accomplish this plan?',
     });
     userEvent.type(taskInput, 'quux');
     fireEvent.keyDown(taskInput, { key: 'Enter', code: 'Enter' });
-    expect(setField).toHaveBeenCalledWith('tasks', mockTasks.concat('quux'));
+    expect(mockDispatch).toHaveBeenCalledWith({ "type": "SET_TASKS", "value": mockTasks.concat('quux') });
   });
 
   it('can delete a task', () => {
-    render(<Tasks tasks={mockTasks} setField={setField} />);
+    render(<Tasks tasks={mockTasks} dispatch={mockDispatch} />);
     fireEvent.click(screen.getAllByRole('button', { name: 'Delete' })[0]);
-    expect(setField).toHaveBeenCalledWith('tasks', mockTasks.slice(1));
+    expect(mockDispatch).toHaveBeenCalledWith({ "type": "SET_TASKS", "value": mockTasks.slice(1) });
   });
 });
