6a76f7135cae4a0f7a2caf538613df8feee68d60
knightsean00 <knightsean00@gmail.com>
2021-06-17 14:14:00 -0700

    Cleanup Breakdown.js (from John Mitchell)

--- src/Components/Breakdown.js
+++ src/Components/Breakdown.js
@@ -2,106 +2,91 @@ import React from 'react';
 import styled from 'styled-components';
 
 const BarContainer = styled.div`
-    display: flex;
-    flex-direction: row;
-    flex-wrap: nowrap;
-    width: 100%;
-    max-width: 100%;
-    height: 1rem;
-    margin-top: 1rem;
-    margin-bottom: 1rem;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  width: 100%;
+  max-width: 100%;
+  height: 1rem;
+  margin-top: 1rem;
+  margin-bottom: 1rem;
 `;
 
 const Bar = styled.div`
-    height: 100%;
+  height: 100%;
 `;
 
 const LabelsContainer = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: flex-end;
-    margin-bottom: 1rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  margin-bottom: 1rem;
 `;
 
 const Label = styled.div`
-    padding-left: 2rem;
-    display: flex;
-    align-items: center;
+  padding-left: 2rem;
+  display: flex;
+  align-items: center;
 `;
 
 const Square = styled.div`
-    width: .75rem;
-    height: .75rem;
-    margin-right: .5rem
+  width: 0.75rem;
+  height: 0.75rem;
+  margin-right: 0.5rem;
 `;
 
-function title (str) {
-    return str[0].toUpperCase() + str.slice(1).toLowerCase();
+function title(str) {
+  return str[0].toUpperCase() + str.slice(1).toLowerCase();
 }
 
-const Breakdown = ({categoryCount, categoryColor}) => {
-    if (typeof(categoryCount) !== 'object' || categoryCount == null) {
-        return;
-    }
-    const barSpacing = .25 //Percentage of width
-    let sortedCategories = [];
-    let total = 0;
+const Breakdown = ({ categoryCount = {}, categoryColor }) => {
+  const totalCount = Object.keys(categoryCount).reduce(
+    (a, b) => a + categoryCount[b],
+    0
+  );
 
-    for (const [k, v] of Object.entries(categoryCount)) {
-        total += v;
-    }
-    for (const [k, v] of Object.entries(categoryCount)) {
-        if (v != 0) {
-            sortedCategories.push([k, v / total, categoryColor[k]]);
-        }   
-    }
-
-    sortedCategories.sort((a, b) => {
-        if (a[1] < b[1]) {
-            return 1; // Normally should be -1, but we want descending order
-        }
-        if (a[1] > b[1]) {
-            return -1;
-        }
-        return 0;
+  const sortedCategories = Object.keys(categoryCount)
+    .filter((category) => categoryCount[category] > 0)
+    .sort((category) => {
+      if (categoryCount[category] < categoryCount[category]) {
+        return 1; // Normally should be -1, but we want descending order
+      }
+      if (categoryCount[category] > categoryCount[category]) {
+        return -1;
+      }
+      return 0;
+    })
+    .map((category) => {
+      return {
+        name: category,
+        bar_spacing: categoryCount[category] / totalCount,
+        color: categoryColor[category],
+      };
     });
 
-    let totalSpace = 1 - (sortedCategories.length * barSpacing / 100);
-
-    return (
-        <>
-            <BarContainer>
-                {
-                    sortedCategories.map((v, idx) => {
-                        if (idx < sortedCategories.length - 1) {
-                            return (
-                                <React.Fragment key={`bars-${idx}`}>
-                                    <Bar style={{backgroundColor: v[2], width:`${v[1] * totalSpace * 100}%`}}/>
-                                    <Bar style={{backgroundColor: "transparent", width:`${barSpacing}%`}}/>
-                                </React.Fragment>
-                            )
-                        } else {
-                            return (
-                                <React.Fragment key={`bars-${idx}`}>
-                                    <Bar style={{backgroundColor: v[2], width:`${v[1] * totalSpace * 100}%`}}/>
-                                </React.Fragment>
-                            )
-                        }
-                    })
-                }
-            </BarContainer>
-            <LabelsContainer>
-                {
-                    sortedCategories.map((v, idx) => (
-                        <Label key={`label-${idx}`}>
-                            <Square style={{backgroundColor: v[2]}} />
-                            <p>{title(v[0])}</p>
-                        </Label>
-                    ))
-                }
-            </LabelsContainer>
-        </>
-    )
-}
+  return (
+    <>
+      <BarContainer>
+        {sortedCategories.map(({ name, bar_spacing, color }) => (
+          <Bar
+            key={name}
+            style={{
+              backgroundColor: color,
+              width: `${bar_spacing * 100}%`,
+            }}
+          />
+        ))}
+      </BarContainer>
+      <LabelsContainer>
+        {sortedCategories.map(({ name, color }) => (
+          <Label key={`label-${name}`}>
+            <Square style={{ backgroundColor: color }} />
+            <p>{title(name)}</p>
+          </Label>
+        ))}
+      </LabelsContainer>
+    </>
+  );
+};
 
 export default Breakdown;
