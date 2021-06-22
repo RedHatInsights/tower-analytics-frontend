2037fb9371ea614e22f2455668eb35bdb3d13bf0
knightsean00 <knightsean00@gmail.com>
2021-06-22 10:09:18 -0700

    Update JobExplorerList to meet linting standards

--- src/Components/JobExplorerList.js
+++ src/Components/JobExplorerList.js
@@ -1,77 +1,80 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
 
-import { 
-  TableComposable, 
-  Thead, 
-  Tbody, 
-  Tr, 
-  Th, 
-  Td, 
-  ExpandableRowContent 
+import {
+  TableComposable,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  ExpandableRowContent,
 } from '@patternfly/react-table';
 
-import LoadingState from '../Components/LoadingState';
 import { formatDateTime, formatJobType } from '../Utilities/helpers';
 import JobStatus from './JobStatus';
 
-const cols = ["Id/Name", "Status", "Cluster", "Organization", "Type"];
+const cols = ['Id/Name', 'Status', 'Cluster', 'Organization', 'Type'];
 
 const JobExplorerList = ({ jobs }) => {
   const [expanded, setExpanded] = useState(Array(jobs.length).fill(false));
   const handleExpansion = (event, idx) => {
     setExpanded({
       ...expanded,
-      [idx]: !expanded[idx]
+      [idx]: !expanded[idx],
     });
-  }
+  };
 
   return (
     <TableComposable aria-label="Job Explorer Table" variant="compact">
       <Thead>
-        <Tr> 
+        <Tr>
           <Th />
-          {
-            cols.map((head, idx) => (
-              <Th key={`col-${idx}`}>{head}</Th>
-            ))
-          }
+          {cols.map((head, idx) => (
+            <Th key={`col-${idx}`}>{head}</Th>
+          ))}
         </Tr>
       </Thead>
       <Tbody>
-        {
-          jobs.map((job, idx) => (
-              <React.Fragment key={job.id.id}>
-                <Tr>
-                  <Td
-                    expand={{
-                      rowIndex: idx,
-                      isExpanded: expanded[idx],
-                      onToggle: handleExpansion
-                    }}
-                  />
-                  <Td href={job.id.tower_link}>{`${job.id.id} - ${job.id.template_name}`}</Td>
-                  <Td><JobStatus status={job.status} /></Td>
-                  <Td>{job.cluster_name}</Td>
-                  <Td>{job.org_name}</Td>
-                  <Td>{formatJobType(job.job_type)}</Td>
-                </Tr>
-                <Tr isExpanded={expanded[idx] === true}>
-                  <Td noPadding={false} colSpan={2}>
-                    <ExpandableRowContent><strong>Created:</strong> {formatDateTime(job.created)}</ExpandableRowContent>
-                  </Td>
-                  <Td noPadding={false} colSpan={2}>
-                    <ExpandableRowContent><strong>Started:</strong> {formatDateTime(job.started)}</ExpandableRowContent>
-                  </Td>
-                  <Td noPadding={false} colSpan={2}>
-                    <ExpandableRowContent><strong>Finished:</strong> {formatDateTime(job.finished)}</ExpandableRowContent>
-                  </Td>
-                </Tr>
-              </React.Fragment>
-            )
-          )
-        }
+        {jobs.map((job, idx) => (
+          <React.Fragment key={job.id.id}>
+            <Tr>
+              <Td
+                expand={{
+                  rowIndex: idx,
+                  isExpanded: expanded[idx],
+                  onToggle: handleExpansion,
+                }}
+              />
+              <Td
+                href={job.id.tower_link}
+              >{`${job.id.id} - ${job.id.template_name}`}</Td>
+              <Td>
+                <JobStatus status={job.status} />
+              </Td>
+              <Td>{job.cluster_name}</Td>
+              <Td>{job.org_name}</Td>
+              <Td>{formatJobType(job.job_type)}</Td>
+            </Tr>
+            <Tr isExpanded={expanded[idx] === true}>
+              <Td noPadding={false} colSpan={2}>
+                <ExpandableRowContent>
+                  <strong>Created:</strong> {formatDateTime(job.created)}
+                </ExpandableRowContent>
+              </Td>
+              <Td noPadding={false} colSpan={2}>
+                <ExpandableRowContent>
+                  <strong>Started:</strong> {formatDateTime(job.started)}
+                </ExpandableRowContent>
+              </Td>
+              <Td noPadding={false} colSpan={2}>
+                <ExpandableRowContent>
+                  <strong>Finished:</strong> {formatDateTime(job.finished)}
+                </ExpandableRowContent>
+              </Td>
+            </Tr>
+          </React.Fragment>
+        ))}
       </Tbody>
     </TableComposable>
   );
