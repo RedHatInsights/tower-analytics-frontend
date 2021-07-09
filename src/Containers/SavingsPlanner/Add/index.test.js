7325bccc8c469c3900c49899d3d58d732bb5fc3d
knightsean00 <knightsean00@gmail.com>
2021-07-09 14:17:33 -0700

    Add @testing-library/jest-dom and improve SavingsPlanner/Add tests

--- src/Containers/SavingsPlanner/Add/index.test.js
+++ src/Containers/SavingsPlanner/Add/index.test.js
@@ -1,30 +1,43 @@
+import '@testing-library/jest-dom';
 import Add from '.';
 import { act } from 'react-dom/test-utils';
+import { screen } from '@testing-library/react';
 import { renderPage } from '../../../Utilities/tests/helpers.reactTestingLib';
-
+import mockResponses from '../../../Utilities/__fixtures__/';
 import * as api from '../../../Api';
 jest.mock('../../../Api');
 
 describe('SavingsPlanner/Add', () => {
-  it('can see the Add component', async () => {
-    api.readPlanOptions.mockResolvedValue({
-      data: { meta: { rbac: { perms: { all: true } } } },
-      isSuccess: true,
-    });
+  beforeEach(() => {
+    api.preflightRequest.mockResolvedValue(mockResponses.preflightRequest200);
+    api.readPlanOptions.mockResolvedValue(mockResponses.readPlansOptions);
+  });
 
+  it('can see the Add component', async () => {
     await act(async () => {
       renderPage(Add);
     });
+
+    expect(screen.getByText('Savings Planner')).toBeTruthy();
+    expect(screen.getByText('Create new plan')).toBeTruthy();
+
+    expect(screen.getAllByText('Details')).toHaveLength(2);
+    expect(screen.getByText('Tasks')).toBeTruthy();
+    expect(screen.getByText('Link template')).toBeTruthy();
   });
 
-  // it('redirects upon failure', async () => {
-  //   api.readPlanOptions.mockResolvedValue({
-  //     data: { meta: { rbac: { perms: { all: false } } } },
-  //     isSuccess: true,
-  //   });
+  it('redirects upon failure', async () => {
+    api.preflightRequest.mockRejectedValue(mockResponses.preflightRequest403);
+    api.readPlanOptions.mockResolvedValue({
+      data: { meta: { rbac: { perms: { all: false } } } },
+      isSuccess: true,
+    });
+    var wrapper;
 
-  //   await act(async () => {
-  //     renderPage(Add);
-  //   });
-  // });
+    await act(async () => {
+      wrapper = renderPage(Add);
+    });
+
+    expect(wrapper.container).toBeEmptyDOMElement();
+  });
 });
