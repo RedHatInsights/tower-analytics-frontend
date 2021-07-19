9558f50374e75ae6d776697b4322b019a5e3dd36
Levente Berky <levente.berky@gmail.com>
2021-07-19 14:23:20 +0200

    Adding the DatePicker with min max date validation to the toolbar

--- src/Components/Toolbar/Groups/QuickDateGroup.js
+++ src/Components/Toolbar/Groups/QuickDateGroup.js
@@ -1,42 +1,63 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { ToolbarGroup } from '@patternfly/react-core';
+import { SplitItem, ToolbarGroup, Split } from '@patternfly/react-core';
 
 import ToolbarInput from './ToolbarInput/';
 
-const QuickDateGroup = ({ filters, handleSearch, setFilters, values }) => (
-  <ToolbarGroup variant="filter-group">
-    <ToolbarInput
-      categoryKey="quick_date_range"
-      value={filters.quick_date_range}
-      selectOptions={values}
-      setValue={(value) => {
-        setFilters('quick_date_range', value);
-        handleSearch('quick_date_range', value);
-      }}
-    />
-    {filters.quick_date_range && filters.quick_date_range.includes('custom') && (
-      <>
-        <ToolbarInput
-          categoryKey="start_date"
-          value={filters.start_date}
-          setValue={(e) => {
-            setFilters('start_date', e);
-            handleSearch('start_date', e);
-          }}
-        />
-        <ToolbarInput
-          categoryKey="end_date"
-          value={filters.end_date}
-          setValue={(e) => {
-            setFilters('end_date', e);
-            handleSearch('end_date', e);
-          }}
-        />
-      </>
-    )}
-  </ToolbarGroup>
-);
+const getDateByDays = (days, format = true) => {
+  const date = new Date();
+  date.setDate(date.getDate() + days);
+  return format ? date.toISOString().split(/T/)[0] : date;
+};
+
+const QuickDateGroup = ({ filters, handleSearch, setFilters, values }) => {
+  const endDate = filters.end_date || getDateByDays(0, true);
+  const startDate = filters.start_date || getDateByDays(-30, true);
+
+  return (
+    <ToolbarGroup variant="filter-group">
+      <ToolbarInput
+        categoryKey="quick_date_range"
+        value={filters.quick_date_range}
+        selectOptions={values}
+        setValue={(value) => setFilters('quick_date_range', value)}
+      />
+      {filters.quick_date_range && filters.quick_date_range.includes('custom') && (
+        <Split hasGutter>
+          <SplitItem>
+            <ToolbarInput
+              categoryKey="start_date"
+              value={startDate}
+              setValue={(e) => {
+                setFilters('start_date', e);
+                handleSearch('start_date', e);
+              }}
+              validators={[
+                (date) =>
+                  date > new Date(endDate) ? 'Must be before end date' : '',
+              ]}
+            />
+          </SplitItem>
+          <SplitItem style={{ paddingTop: '6px' }}>to</SplitItem>
+          <SplitItem>
+            <ToolbarInput
+              categoryKey="end_date"
+              value={endDate}
+              setValue={(e) => {
+                setFilters('end_date', e);
+                handleSearch('end_date', e);
+              }}
+              validators={[
+                (date) =>
+                  date < new Date(startDate) ? 'Must be after start date' : '',
+              ]}
+            />
+          </SplitItem>
+        </Split>
+      )}
+    </ToolbarGroup>
+  );
+};
 
 QuickDateGroup.propTypes = {
   filters: PropTypes.object.isRequired,
