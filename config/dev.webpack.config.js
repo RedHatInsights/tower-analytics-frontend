942641c55ab11837bbf8e02fc339c820c37b558e
Levente Berky <levente.berky@gmail.com>
2021-07-19 15:36:55 +0200

    Adding the standalone to the development env

--- config/dev.webpack.config.js
+++ config/dev.webpack.config.js
@@ -1,11 +1,25 @@
 const { resolve } = require('path');
 const config = require('@redhat-cloud-services/frontend-components-config');
+const {
+  rbac,
+  defaultServices,
+} = require('@redhat-cloud-services/frontend-components-config-utilities/standalone');
+
 const { config: webpackConfig, plugins } = config({
   rootFolder: resolve(__dirname, '../'),
   debug: true,
-  https: true,
   sassPrefix: '.automation-analytics, .automationAnalytics',
   useCloud: true,
+  https: false,
+  standalone: {
+    apiAnalytics: {
+      context: ['/api/tower-analytics'],
+      target: 'http://localhost:8004',
+    },
+    rbac,
+    ...defaultServices,
+  },
+  appUrl: ['/beta/ansible/insights/', '/ansible/insights/'],
   ...(process.env.BETA && { deployment: 'beta/apps' }),
 });
 
