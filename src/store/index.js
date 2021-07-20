ca3f931c4483ae175e98b40ef672f5b9cd6b60b7
Martin Marosi <marvusm.mmi@gmail.com>
2021-07-20 10:31:25 +0200

    Enable chrome 2

--- src/store/index.js
+++ src/store/index.js
@@ -4,11 +4,9 @@ import promiseMiddleware from 'redux-promise-middleware';
 let registry;
 
 export function init(...middleware) {
-  if (registry) {
-    throw new Error('store already initialized');
+  if (!registry) {
+    registry = new ReducerRegistry({}, [promiseMiddleware, ...middleware]);
   }
-
-  registry = new ReducerRegistry({}, [promiseMiddleware, ...middleware]);
 
   //If you want to register all of your reducers, this is good place.
   /*
