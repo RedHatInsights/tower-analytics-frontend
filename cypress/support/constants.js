4949a7aa135c018402f188478e38aae6f4d749dd
Levente Berky <levente.berky@gmail.com>
2021-07-21 12:49:53 +0200

    Cypress: get cypress up and running again with keycloak

---
+++ cypress/support/constants.js
@@ -0,0 +1,7 @@
+/* global Cypress */
+export const appid = Cypress.env('appid');
+export const calculatorUrl = '/automation-calculator';
+export const jobExplorerUrl = '/job-explorer';
+export const dashboardUrl = '/clusters';
+export const notificationsUrl = '/notifications';
+export const orgsUrl = '/organization-statistics';
