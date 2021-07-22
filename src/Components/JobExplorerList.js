6664e8566054d4e87deb7a7d8b6c1ad2c362b55e
knightsean00 <knightsean00@gmail.com>
2021-07-22 14:45:34 -0700

    Finalize column sorting for JobExplorerTable

--- src/Components/JobExplorerList.js
+++ src/Components/JobExplorerList.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
 import { TableComposable, Thead, Tbody, Tr, Th } from '@patternfly/react-table';
@@ -12,7 +12,7 @@ const sortMap = {
   Type: 'job_type',
 };
 
-const reverseSortMap = {
+const idxSortMap = {
   id: 0,
   status: 1,
   job_type: 4,
@@ -22,12 +22,21 @@ const JobExplorerList = ({ jobs, queryParams, queryParamsDispatch }) => {
   const [sortIdx, setSortIdx] = useState(-1);
   const [sortDir, setSortDir] = useState('none');
 
-  if (queryParams?.sort_options in reverseSortMap) {
-    setSortIdx(reverseSortMap[queryParams.sort_options]);
-    setSortDir(queryParams?.sort_order);
-    console.log(sortIdx);
-    console.log(sortDir);
-  }
+  useEffect(() => {
+    if (queryParams?.sort_options in idxSortMap) {
+      setSortIdx(idxSortMap[queryParams.sort_options]);
+      setSortDir(
+        queryParams?.sort_order &&
+          (queryParams.sort_order === 'desc' ||
+            queryParams.sort_order === 'asc')
+          ? queryParams.sort_order
+          : 'desc'
+      );
+    } else {
+      setSortIdx(-1);
+      setSortDir('none');
+    }
+  }, [queryParams]);
 
   const onSort = (_event, idx, dir) => {
     if (idx !== sortIdx) {
