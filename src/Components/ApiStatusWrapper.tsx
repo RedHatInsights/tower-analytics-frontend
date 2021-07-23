d034f22ac4769ef5938c8766787c71e894edab38
Levente Berky <levente.berky@gmail.com>
2021-07-23 07:56:18 +0200

    Ts postfixes, and preventing status bar from reloading

---
+++ src/Components/ApiStatusWrapper.tsx
@@ -0,0 +1,36 @@
+import React, { FunctionComponent } from 'react';
+import PropTypes from 'prop-types';
+import ApiErrorState from './ApiErrorState';
+import LoadingState from './LoadingState';
+import NoData from './NoData';
+
+interface Props {
+  api: {
+    result: unknown;
+    error: { error: string; [key: string]: string } | null;
+    isSuccess: boolean;
+    isLoading: boolean;
+  };
+  children: React.ReactNode;
+}
+
+const ApiStatusWrapper: FunctionComponent<Props> = ({ api, children }) => {
+  if (!api || api.isLoading) return <LoadingState />;
+  if (api.error) return <ApiErrorState message={api.error.error} />;
+
+  if (api.isSuccess) {
+    if (Array.isArray(api.result) && api.result.length === 0) return <NoData />;
+    if (Object.keys(api.result as Record<string, string>).length === 0)
+      return <NoData />;
+    return <>{children}</>;
+  }
+
+  return null;
+};
+
+ApiStatusWrapper.propTypes = {
+  api: PropTypes.any.isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+export default ApiStatusWrapper;
