4f492f41823a5786b80cd1b99cdeb1beeb507a97
Levente Berky <levente.berky@gmail.com>
2021-08-07 21:44:56 +0200

    Converted date selector to ts

--- src/Components/Toolbar/Groups/ToolbarInput/index.tsx
+++ src/Components/Toolbar/Groups/ToolbarInput/index.tsx
@@ -22,6 +22,7 @@ interface Props {
   selectOptions?: SelectOptionProps[];
   isVisible?: boolean;
   setValue: SetValue;
+  [x: string]: any;
 }
 
 const ToolbarInput: FunctionComponent<Props> = ({
