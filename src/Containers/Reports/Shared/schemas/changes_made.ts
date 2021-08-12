93ff88ce8d8de35e81efa8ad292f84eecd7a2721
knightsean00 <knightsean00@gmail.com>
2021-08-12 13:02:19 -0700

    Add tracking tooltip for all points

--- src/Containers/Reports/Shared/schemas/changes_made.ts
+++ src/Containers/Reports/Shared/schemas/changes_made.ts
@@ -85,7 +85,13 @@ const schemaFnc = (label: string, y: string): ChartSchemaElement[] => [
       orientation: ChartLegendOrientation.vertical,
       position: ChartLegendPosition.right,
     },
-    tooltip: {},
+    tooltip: {
+      mouseFollow: true,
+      stickToAxis: 'x',
+      cursor: true,
+      customFnc: (datum: Record<string, string | number>) =>
+        `${datum.name}: ${datum.y}`,
+    },
   },
   {
     id: 2,
@@ -99,9 +105,6 @@ const schemaFnc = (label: string, y: string): ChartSchemaElement[] => [
       props: {
         x: 'created_date',
         y,
-      },
-      tooltip: {
-        standalone: true,
       },
     },
   },
