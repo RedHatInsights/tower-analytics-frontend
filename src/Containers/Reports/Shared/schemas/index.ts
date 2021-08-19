4ae65c0ef7621c522e1694927dbf621cb20be8ab
Levente Berky <levente.berky@gmail.com>
2021-08-19 11:00:55 +0200

    Added orgs page charts as reports

--- src/Containers/Reports/Shared/schemas/index.ts
+++ src/Containers/Reports/Shared/schemas/index.ts
@@ -2,8 +2,16 @@ import { ReportPageParams } from '../types';
 import affectedHostsByPlaybook from './affectedHostsByPlaybook';
 import changesMade from './changesMade';
 import playbookRunRate from './playbookRunRate';
+import hostsByOrganization from './hostsByOrganizations';
+import jobsTasksByOrganization from './jobsTasksByOrganization';
 
-const reports = [affectedHostsByPlaybook, changesMade, playbookRunRate];
+const reports = [
+  affectedHostsByPlaybook,
+  changesMade,
+  playbookRunRate,
+  hostsByOrganization,
+  jobsTasksByOrganization,
+];
 
 const defaultReport: ReportPageParams = {
   slug: 'unknown',
