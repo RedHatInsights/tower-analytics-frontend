7262e85d170acbb3cba93e9ad35b8f33cbf6dd24
Levente Berky <levente.berky@gmail.com>
2021-09-11 10:29:38 +0200

    Feature flags are now always enabled on beta

--- src/FeatureFlags/useFeatureFlag.tsx
+++ src/FeatureFlags/useFeatureFlag.tsx
@@ -2,7 +2,13 @@ import { useContext } from 'react';
 import Context from './Context';
 import { ValidFeatureFlags } from './types';
 
+const isBeta = () => window.location.pathname.split('/')[1] === 'beta';
+
 const useFeatureFlag = (flag: ValidFeatureFlags): boolean => {
+  // On beta features are always enabled.
+  if (isBeta()) return true;
+
+  // On prod check the feature flag.
   const features = useContext(Context);
   const feature = features.find(({ name }) => name === flag);
   return !!feature && feature.enabled;
