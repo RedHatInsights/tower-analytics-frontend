9fc0c3177201b3908fddac63f47a11f73877bb50
Levente Berky <levente.berky@gmail.com>
2021-09-27 11:02:39 +0200

    Add test for QueryParams - partly from old qs

---
+++ src/QueryParams/Context.test.js
@@ -0,0 +1,8 @@
+import { QueryParamsContext, QueryParamsProvider } from './Context';
+
+describe('QueryParams/Context', () => {
+  it('should have exported members', () => {
+    expect(QueryParamsContext).toBeDefined();
+    expect(QueryParamsProvider).toBeDefined();
+  });
+});
