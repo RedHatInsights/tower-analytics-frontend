e1f477e0984d7d9f8b19115de41972053ce8f961
Mat Wilson <mawilson@redhat.com>
2021-10-18 13:35:17 -0700

    initial reports smoke tests

--- cypress/integration/Reports.spec.js
+++ cypress/integration/Reports.spec.js
@@ -15,7 +15,90 @@ describe('Reports page smoketests', () => {
     cy.visit(reportsUrl);
   });
 
-  it('All report cards are displayed on main reports page', () => {
-    // Add more once fixtures are implemented - other filters are content-dependent.
+  it.skip('All report cards are displayed on main reports page', () => {});
+});
+
+describe('Report: Hosts Changed By Job Template Smoketests', () => {
+  beforeEach(() => {
+    cy.loginFlow();
+    cy.visit(reportsUrl + '/' + hcbjt);
+  });
+
+  it('Can Switch between Line and Bar chart without breaking UI', () => {
+    cy.get('#bar').click();
+    cy.screenshot('report_hcbjt_bar.png');
+    cy.get('#line').click();
+    cy.screenshot('report_hcbjt_line.png');
+  });
+
+  it('Can change lookback without breaking the UI', () => {
+    cy.get('[data-cy="quick_date_range"]').click();
+  });
+});
+
+describe('Report: Changes Made By Job Template Smoketests', () => {
+  beforeEach(() => {
+    cy.loginFlow();
+    cy.visit(reportsUrl + '/' + cmbjt);
+  });
+
+  it('Can Switch between Line and Bar chart without breaking UI', () => {
+    cy.get('#bar').click();
+    cy.screenshot('report_cmbjt_bar.png');
+    cy.get('#line').click();
+    cy.screenshot('report_cmbjt_line.png');
+  });
+});
+
+describe('Report: Job Template Run Rate Smoketests', () => {
+  beforeEach(() => {
+    cy.loginFlow();
+    cy.visit(reportsUrl + '/' + jtrr);
+  });
+
+  it('Can Switch between Line and Bar chart without breaking UI', () => {
+    cy.get('#bar').click();
+    cy.screenshot('report_jtrr_bar.png');
+    cy.get('#line').click();
+    cy.screenshot('report_jtrr_line.png');
+  });
+});
+
+describe('Report: Hosts By Organization Smoketests', () => {
+  beforeEach(() => {
+    cy.loginFlow();
+    cy.visit(reportsUrl + '/' + hbo);
+  });
+
+  it('Can Switch between Line and Bar chart without breaking UI', () => {
+    cy.get('#bar').click();
+    cy.screenshot('report_hbo_bar.png');
+    cy.get('#line').click();
+    cy.screenshot('report_hbo_line.png');
+  });
+});
+
+describe('Report: Jobs and Tasks By Organization Smoketests', () => {
+  beforeEach(() => {
+    cy.loginFlow();
+    cy.visit(reportsUrl + '/' + jtbo);
+  });
+
+  it('Can Switch between Line and Bar chart without breaking UI', () => {
+    cy.get('#bar').click();
+    cy.screenshot('report_jtbo_bar.png');
+    cy.get('#line').click();
+    cy.screenshot('report_jtbo_line.png');
+  });
+});
+
+describe('Report: Templates Explorer Smoketests', () => {
+  beforeEach(() => {
+    cy.loginFlow();
+    cy.visit(reportsUrl + '/' + texp);
+  });
+
+  it('Can change lookback without breaking the UI', () => {
+    cy.get('[data-cy="quick_date_range"]').click();
   });
 });
