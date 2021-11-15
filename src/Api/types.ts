e6da75d31c5c82000ed670625b050a2536e7bc22
Salma Kochay <skochay330@gmail.com>
2021-11-15 12:07:44 -0500

    showExtraRows boolean

--- src/Api/types.ts
+++ src/Api/types.ts
@@ -24,7 +24,7 @@ export interface PDFParams {
   y: string;
   label: string;
   x_tick_format: string;
-  optionSelected: string;
+  showExtraRows: boolean;
 }
 
 export type ReadParams = { params: Params };
