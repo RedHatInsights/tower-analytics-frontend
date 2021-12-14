c542f011fb2b1727a041270c63b54cbaf81ca876
Levente Berky <levente.berky@gmail.com>
2021-12-14 16:09:59 +0100

    Change the logic how the FF works in devel env
    And add the ephemeral environment as a devel environment

--- src/FeatureFlags/useFeatureFlag.ts
+++ src/FeatureFlags/useFeatureFlag.ts
@@ -1,9 +1,33 @@
 import { useContext } from 'react';
 import Context from './Context';
-import { ValidFeatureFlags } from './types';
+import { FeatureFlagType, ValidFeatureFlags } from './types';
 
 const isBeta = () => window.location.pathname.split('/')[1] === 'beta';
+
+// Devel environment checks
 const isLocalhost = () => window.location.hostname === 'localhost';
+const isEphemeral = () => window.location.hostname.includes('ephemeral');
+
+/**
+ * On ephemeral and local environments change how the flags work:
+ * If not defined => enabled
+ * If defined and true => enabled
+ * If defined and false => disabled
+ */
+const isEnabledDevel = (feature?: FeatureFlagType) => {
+  if (!feature) return true;
+  return feature?.enabled;
+};
+
+/**
+ * In non developer environment enable the feature flag as:
+ * If not defined => disabled
+ * If defined and true => enabled
+ * If defined and false => disabled
+ */
+const isEnabled = (feature?: FeatureFlagType) => {
+  return !!feature && feature?.enabled;
+};
 
 const useFeatureFlag = (flag: ValidFeatureFlags): boolean => {
   const features = useContext(Context);
@@ -15,8 +39,9 @@ const useFeatureFlag = (flag: ValidFeatureFlags): boolean => {
     ({ name }) => name === (isBeta() ? betaFlag : flag)
   );
 
-  if (isLocalhost()) return true;
-  return !!feature && feature.enabled;
+  if (isLocalhost() || isEphemeral()) return isEnabledDevel(feature);
+
+  return isEnabled(feature);
 };
 
 export default useFeatureFlag;
