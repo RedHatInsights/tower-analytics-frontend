7be75ba30ae5aee7190142324d059bab3ed75da6
James Wong <2421248+jameswnl@users.noreply.github.com>
2021-12-16 22:46:26 -0500

    streamlined pr check script

--- .travis/custom_release.sh
+++ .travis/custom_release.sh
@@ -2,31 +2,34 @@
 set -e
 set -x
 
-if [[ "${TRAVIS_PULL_REQUEST_BAK}" != "false" && "${PR_CHECK}" == "true" ]]; then
+if [ "${TRAVIS_PULL_REQUEST_BAK}" != "false" ]; then
+    # This is a PR build.
     # reset TRAVIS_PULL_REQUEST
+    git log -n 3
+    echo `git log -n 3`
+    echo `git rev-parse --verify HEAD`
     TRAVIS_PULL_REQUEST="${TRAVIS_PULL_REQUEST_BAK}"
     export TRAVIS_PULL_REQUEST
     .travis/release.sh "PR-${TRAVIS_PULL_REQUEST}"
     exit $?
 fi
 
-echo 'problem ****************************'
 # If current dev branch is devel, push to build repo ci, qa, and prod-beta
 # qa is stage, ci is probably not used anymore
-# if [ "${TRAVIS_BRANCH}" = "devel" ]; then
-#     .travis/release.sh "ci-beta"
-#     rm -rf dist/.git
-#     .travis/release.sh "ci-stable"
-#     rm -rf dist/.git
-#     .travis/release.sh "qa-beta"
-#     rm -rf dist/.git
-#     .travis/release.sh "qa-stable"
-#     rm -rf dist/.git
-#     .travis/release.sh "prod-beta"
-#     rm -rf dist/.git
-# fi
+if [ "${TRAVIS_BRANCH}" = "devel" ]; then
+    .travis/release.sh "ci-beta"
+    rm -rf dist/.git
+    .travis/release.sh "ci-stable"
+    rm -rf dist/.git
+    .travis/release.sh "qa-beta"
+    rm -rf dist/.git
+    .travis/release.sh "qa-stable"
+    rm -rf dist/.git
+    .travis/release.sh "prod-beta"
+    rm -rf dist/.git
+fi
 
-# # If current dev branch is deployment branch, push to build repo
-# if [ "${TRAVIS_BRANCH}" = "prod-stable" ]; then
-#     .travis/release.sh "${TRAVIS_BRANCH}"
-# fi
+# If current dev branch is deployment branch, push to build repo
+if [ "${TRAVIS_BRANCH}" = "prod-stable" ]; then
+    .travis/release.sh "${TRAVIS_BRANCH}"
+fi
--- .travis/pr_build.sh
+++ .travis/pr_build.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 echo "PR: ${TRAVIS_PULL_REQUEST}"
-echo "PR_CHECK: ${PR_CHECK}"
 echo "PR_BRANCH: BRANCH=${BRANCH}, TRAVIS_PULL_REQUEST_BRANCH=${TRAVIS_PULL_REQUEST_BRANCH}, TRAVIS_BRANCH=${TRAVIS_BRANCH}"
-TRAVIS_PULL_REQUEST_BAK="${TRAVIS_PULL_REQUEST}"
-export TRAVIS_PULL_REQUEST_BAK
-export TRAVIS_PULL_REQUEST=false
-echo "Backup to TRAVIS_PULL_REQUEST_BAK: ${TRAVIS_PULL_REQUEST_BAK}"
 
-npm run deploy-only
-curl -sSL https://raw.githubusercontent.com/RedHatInsights/insights-frontend-builder-common/master/src/bootstrap.sh > bootstrap.sh
-chmod +x bootstrap.sh
-bash bootstrap.sh
+if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
+  # if this is a PR build, backup TRAVIS_PULL_REQUEST and set it to false
+  # will be reset at end of custom_release.sh
+  TRAVIS_PULL_REQUEST_BAK="${TRAVIS_PULL_REQUEST}"
+  export TRAVIS_PULL_REQUEST_BAK
+  export TRAVIS_PULL_REQUEST=false
+  echo "Backup to TRAVIS_PULL_REQUEST_BAK: ${TRAVIS_PULL_REQUEST_BAK}"
+fi
+npm run deploy-only && curl -sSL https://raw.githubusercontent.com/RedHatInsights/insights-frontend-builder-common/master/src/bootstrap.sh | bash -s
