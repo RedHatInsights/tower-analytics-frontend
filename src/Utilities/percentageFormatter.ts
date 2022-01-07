2b9702c3e19bc18175095ee6388a4e717ecb3f81
Zita Nemeckova <znemecko@redhat.com>
2022-01-07 14:33:44 +0100

    The column that changes based on the selected sort does include label

---
+++ src/Utilities/percentageFormatter.ts
@@ -0,0 +1,6 @@
+const percentageFormatter = (n: number): string => {
+  const formatter = new Intl.NumberFormat('en-US', { maximumSignificantDigits: 2 });
+
+  return formatter.format(n);
+};
+export default percentageFormatter;
