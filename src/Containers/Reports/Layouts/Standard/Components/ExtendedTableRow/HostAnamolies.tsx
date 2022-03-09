2c0840dd4553c06693dae41467f814a6442c405d
jeanetteobr <jobrien@redhat.com>
2022-03-09 16:51:55 -0500

    Add component for rendering expanded row information on host anamolies report table.

---
+++ src/Containers/Reports/Layouts/Standard/Components/ExtendedTableRow/HostAnamolies.tsx
@@ -0,0 +1,55 @@
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+import React from 'react';
+import {
+  DescriptionList,
+  DescriptionListDescription,
+  DescriptionListGroup,
+  DescriptionListTerm,
+} from '@patternfly/react-core';
+import { ExpandableRowContent, Td, Tr } from '@patternfly/react-table';
+
+const TableExpandedRow: ExpandedTableRowComponent = ({ isExpanded, item }) => {
+  const expandedInfo = (item: any) => {
+    return [
+      {
+        label: 'Slow Hosts Count',
+        value: item.slow_hosts_count ?? 0,
+      },
+      {
+        label: 'Host Task Count',
+        value: item.host_task_count ?? 0,
+      },
+      {
+        label: 'Slow Hosts Percentage',
+        value: `${item.slow_hosts_percentage}%` ?? 0,
+      },
+      {
+        label: 'Template Success Rate',
+        value: `${item.template_success_rate}%` ?? 0,
+      },
+    ];
+  };
+
+  return (
+    <Tr isExpanded={isExpanded}>
+      <Td colSpan={6}>
+        <ExpandableRowContent>
+          <DescriptionList isHorizontal columnModifier={{ lg: '3Col' }}>
+            {expandedInfo(item).map(({ label, value }) => (
+              <DescriptionListGroup key={label}>
+                <DescriptionListTerm>{label}</DescriptionListTerm>
+                <DescriptionListDescription>{value}</DescriptionListDescription>
+              </DescriptionListGroup>
+            ))}
+          </DescriptionList>
+        </ExpandableRowContent>
+      </Td>
+    </Tr>
+  );
+};
+
+export default TableExpandedRow;
