4a73f7e2c969a1f0d610c6108bc37600453ed018
Harpreet Kataria <hkataria@redhat.com>
2022-04-19 11:20:03 -0400

    Side nav changes
    - Move our apps in a collapsible “Automation Analytics” section
    - changed our urls from ansible/insights/ to ansible/automation-analytics
    
    https://issues.redhat.com/browse/AA-1114

--- profiles/local-frontend-and-api.js
+++ profiles/local-frontend-and-api.js
@@ -5,8 +5,10 @@ const routes = {};
 
 routes[`/beta/apps/${APP_ID}`] = { host: `https://localhost:${FRONTEND_PORT}` };
 routes[`/apps/${APP_ID}`] = { host: `https://localhost:${FRONTEND_PORT}` };
-routes[`/ansible/insights`] = { host: `https://localhost:${FRONTEND_PORT}` };
-routes[`/beta/ansible/insights`] = {
+routes[`/ansible/automation-analytics`] = {
+  host: `https://localhost:${FRONTEND_PORT}`,
+};
+routes[`/beta/ansible/automation-analytics`] = {
   host: `https://localhost:${FRONTEND_PORT}`,
 };
 routes[`/beta/config/main.yml`] = { host: `http://localhost:8889` };
