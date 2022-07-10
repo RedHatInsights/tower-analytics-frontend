31a129cb07c1ee687a6e956a43ce05a4930c1c8a
Ingrid Sena <10999758+fullsushidev@users.noreply.github.com>
2022-07-10 20:56:39 +0200

    [cypress] Include find helpers
    Reusing code from tower-qa

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -149,3 +149,101 @@ Cypress.Commands.add('loginFlow', () => {
 
   cy.url().should('eq', Cypress.config().baseUrl + dashboardUrl);
 });
+
+/**
+ * This command get an element using data-ouia-component-id
+ * exact match.
+ *
+ * Example usage:
+ * cy.getByOUIA("inventory-test")
+ *
+ * @param {String} selector - The selector value
+ **/
+ Cypress.Commands.add('getByOUIA', (selector, ...args) => {
+  return cy.get(`[data-ouia-component-id="${selector}"]`, ...args)
+})
+
+/**
+ * This command get an element using data-ouia-component-id
+ * using a partial match anywhere in the element.
+ *
+ * Example usage:
+ * cy.getByOUIALike("inventory-test")
+ *
+ * @param {String} selector - The selector value
+ **/
+Cypress.Commands.add('getByOUIALike', (selector, ...args) => {
+  return cy.get(`[data-ouia-component-id*="${selector}"]`, ...args)
+})
+
+/**
+ * This command get an element using data-cy
+ * exact match.
+ *
+ * Example usage:
+ * cy.getByCy("inventory-test")
+ *
+ * @param {String} selector - The selector value
+ **/
+
+Cypress.Commands.add('getByCy', (selector, ...args) => {
+  return cy.get(`[data-cy="${selector}"]`, ...args)
+})
+
+/**
+ * This command get an element using data-cy
+ * using a partial match anywhere in the element.
+ *
+ * See more on: https://api.jquery.com/category/selectors/attribute-selectors/
+ *
+ * Example usage:
+ * cy.getByCyLike("inventory-test")
+ *
+ * @param {String} selector - The selector value
+ * @param {String} matchType - The operator value
+ * @param {String} args - The operator value to partially find an element.
+ * See more options on https://api.jquery.com/category/selectors/attribute-selectors/
+ **/
+
+Cypress.Commands.add('getByCyLike', (selector, matchType = '*', ...args) => {
+  return matchType == '*'
+    ? cy.get(`[data-cy*="${selector}"]`, ...args)
+    : cy.get(`[data-cy${matchType}="${selector}"]`, ...args)
+})
+
+/**
+ * This command get an element using id
+ * using a partial match anywhere in the element.
+ *
+ * Example usage:
+ * cy.getByIdLike("select-option-description")
+ *
+ * @param {String} selector - The selector value
+ **/
+Cypress.Commands.add('getByIdLike', (selector, ...args) => {
+  return cy.get(`[id*="${selector}"]`, ...args)
+})
+
+/** This command allows the user to enter a locator that uses a data-ouia-component-id, a data-cy, or an id, and find that locator
+ * with the one command.
+ *
+ * User simply passes in the locator string and Cypress will find that locator.
+ * Example: cy.findByCustomId('locator')
+ *
+ * @param {String} idToFind - user inserts the locator here.
+ */
+Cypress.Commands.add('findByCustomId', (idToFind) => {
+  const { queryHelpers } = require('@testing-library/dom')
+  let queryAllByOuia = queryHelpers.queryAllByAttribute.bind(null, 'data-ouia-component-id')
+  let queryAllByDataCy = queryHelpers.queryAllByAttribute.bind(null, 'data-cy')
+  let queryAllById = queryHelpers.queryAllByAttribute.bind(null, 'id')
+
+  let resultA = queryAllByOuia(Cypress.$('body')[0], idToFind)
+  let resultB = queryAllByDataCy(Cypress.$('body')[0], idToFind)
+  let resultC = queryAllById(Cypress.$('body')[0], idToFind)
+  if (resultA.length) return resultA
+  if (resultB.length) return resultB
+  if (resultC.length) return resultC
+
+  throw `Unable to find an element by: [data-ouia-component-id="${idToFind}"] or [data-cy="${idToFind}"] or [id="${idToFind}"]`
+})
