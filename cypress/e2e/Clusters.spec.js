f5a5076ae987c27c178407d50f1161bfeaa1699a
Harpreet Kataria <hkataria@redhat.com>
2022-07-28 09:16:46 -0400

    Added Cypress tests for Clusters page
    - Added `data-cy` attributes to be used in tests

--- cypress/e2e/Clusters.spec.js
+++ cypress/e2e/Clusters.spec.js
@@ -6,12 +6,11 @@ describe('Clusters page', () => {
   beforeEach(() => {
     cy.loginFlow();
     cy.visit(clustersUrl);
-
+    cy.get('[data-cy="spinner"]').should('not.exist');
+    cy.get('[data-cy="loading"]').should('not.exist');
     cy.get('[data-cy="header-clusters"]', {
       timeout: 10000,
     }).should('be.visible');
-    cy.get('[data-cy="spinner"]').should('not.exist');
-    cy.get('[data-cy="loading"]').should('not.exist');
     cy.get('[data-cy="filter-toolbar"]', {
       timeout: 10000,
     }).should('be.visible');
@@ -21,36 +20,71 @@ describe('Clusters page', () => {
     cy.intercept('/api/tower-analytics/v1/event_explorer/*').as(
       'eventExplorerData'
     );
-    cy.intercept('/api/tower-analytics/v1/job_explorer/*').as(
-      'jobExplorerData'
-    );
   });
-
-  const waitToLoad = () => {
-    cy.wait('@eventExplorerData');
-    cy.wait('@jobExplorerData');
-  };
 
   it('loads clusters page with Bar graph and other tables', () => {
     cy.get('[data-cy="card-title-job-status"]').find('h2').textContent;
-
-    cy.get('[data-cy="barchart"]');
-    cy.get('#d3-bar-chart-root').type('g');
-    waitToLoad();
-    cy.contains('Clear all filters');
+    cy.contains('Clear all filters').should('exist');
     cy.contains('Top workflows');
     cy.contains('Top templates');
     cy.contains('Top modules');
+    cy.get('.pf-c-empty-state__content').should('not.exist');
+    cy.get('[data-cy="card-title-job-status"]')
+      .find('h2')
+      .contains('Job status')
+      .should('exist');
+    cy.get('[data-cy="barchart"]').should('exist');
+    cy.get('#d3-bar-chart-root').should('exist');
+    cy.get('button').contains('Clear all filters').should('exist');
+    cy.get('h3').contains('Top workflows').should('exist');
+    cy.get('h3').contains('Top templates').should('exist');
+    cy.get('h3').contains('Top modules').should('exist');
+  });
+
+  it('verifies number of items in API response', () => {
+    cy.get('[data-cy="card-title-job-status"]').find('h2').textContent;
+
+    cy.get('[data-cy="barchart"]');
+    cy.get('#d3-bar-chart-root');
+    cy.get('div.pf-c-empty-state__content').should('not.exist');
+    // get the same request object again and confirm the response
+    cy.get('@eventExplorerData')
+      .its('response')
+      .then((res) => {
+        expect(res.body.items).to.length(5);
+        expect(res.body.meta.count).to.eq(5);
+        expect(res.body.meta.legend).to.length(5);
+      });
   });
 
   it('has anchor clear filters link', () => {
-    waitToLoad();
-    cy.get('button').each((button) => {
-      let buttonText = button.text();
-      if (buttonText === 'Clear all filters') {
-        button.click();
-      }
-    });
+    cy.get('div.pf-c-empty-state__content').should('not.exist');
+
+    cy.get('[data-cy="filter-toolbar"')
+      .find('button')
+      .contains('Clear all filters')
+      .click({ force: true });
+    cy.get('.pf-c-empty-state__content').should('not.exist');
+    // Add Cluster to the filters
+    cy.get('[data-cy="category_selector"]').find('button').click();
+    cy.get('button').contains('Cluster').click();
+    cy.get('[data-cy="cluster_id"]').find('button').click();
+    cy.get('[data-cy="1811"]').find('input').check();
+    // Wait for loading and check the selected filter is present
+    cy.get('.pf-c-empty-state__content').should('not.exist');
+    cy.get('.pf-c-chip-group__main').contains('Cluster').should('exist');
+    cy.get('.pf-c-chip-group__main').contains('10.0.110.68').should('exist');
+
+    cy.get('[data-cy="filter-toolbar"')
+      .find('button')
+      .contains('Clear all filters')
+      .click({ force: true });
+    // Wait and check that the filter is no longer present
+    cy.get('.pf-c-empty-state__content').should('not.exist');
+    cy.get('.pf-c-chip-group__main').contains('Cluster').should('not.exist');
+    cy.get('.pf-c-chip-group__main')
+      .contains('10.0.110.68')
+      .should('not.exist');
   });
 
   it('Query parameters are stored in the URL to enable refresh', () => {
@@ -68,9 +102,10 @@ describe('Clusters page', () => {
       .find('#d3-bar-chart-root > svg > g > g > path')
       .each((_slice, ix) => {
         cy.get(appid)
-          .find('#d3-bar-chart-root > svg > g > g > path')
+          .find('#d3-bar-chart-root > svg > g > g > rect')
           .eq(ix)
           .trigger('mouseover', { force: true });
+        cy.get('[id^=svg-chart-Tooltip]').should('have.css', 'opacity', '1');
       });
   });
 
