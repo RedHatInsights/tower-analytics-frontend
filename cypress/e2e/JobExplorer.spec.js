af28200685fdc6d367d1fa492694e601f19db6e4
Ingrid Sena <10999758+fullsushidev@users.noreply.github.com>
2022-08-22 02:19:19 +0200

    [cypress] Move nav/pag test code
    to be page agnostic.

--- cypress/e2e/JobExplorer.spec.js
+++ cypress/e2e/JobExplorer.spec.js
@@ -16,51 +16,14 @@ describe('Job Explorer page smoketests', () => {
   });
 
   it('Can navigate through the pages', () => {
-    // TODO: navigate through ALL pages
-    
-    cy.getPaginationBtn('top_pagination', 'next').as('nextBtn')
-    cy.getPaginationBtn('top_pagination', 'previous').as('previousBtn')
-    
-    cy.get('@previousBtn').should('be.disabled')
-    cy.get('@nextBtn').should('not.be.disabled').click()
-    
-    cy.getPaginationBtn('top_pagination', 'next').as('nextBtn')
-    cy.getPaginationBtn('top_pagination', 'previous').as('previousBtn')
+    cy.testNavArrowsFlow('top_pagination')
+    cy.testNavArrowsFlow('pagination_bottom')
 
-    cy.get('@nextBtn').should('not.be.disabled')
-    cy.get('@previousBtn').should('not.be.disabled').click()
   });
 
   it('Can change the number of items shown on the list', () => {
-    // TODO: test all values of items per page
-
-    // toggle the list
-    cy.getByCy('top_pagination').find('.pf-c-options-menu').as('top_pag_option_menu');
-    cy.findByIdLike('@top_pag_option_menu', 'aa-pagination-toggle')
-    .should('have.attr', 'aria-expanded', 'false').click()
+    cy.testItemsListFlow('top_pagination')
+    cy.testItemsListFlow('pagination_bottom')
     
-    cy.findByIdLike('@top_pag_option_menu', 'aa-pagination-toggle')
-    .should('have.attr', 'aria-expanded', 'true')
-    
-    // get table and amount of lines
-    cy.get('tbody').find('tr').should('have.length', 10)
-    
-    // assert the options available
-    cy.get('@top_pag_option_menu')
-    .find('ul', 'per-page-5').should(($ul) => {
-        const n = parseFloat($ul.text())
-        expect(n).to.be.eq(5)
-      }).within(($ul) => {
-          cy.get('li').eq(0).find('button').should('have.attr', 'data-action').and('include', 'per-page-5')
-          cy.get('li').eq(1).find('button').should('have.attr', 'data-action').and('include', 'per-page-10')
-          cy.get('li').eq(2).find('button').should('have.attr', 'data-action').and('include', 'per-page-15')
-          cy.get('li').eq(3).find('button').should('have.attr', 'data-action').and('include', 'per-page-20')
-          cy.get('li').eq(4).find('button').as('maxItems')
-          cy.get('@maxItems').should('have.attr', 'data-action')
-          .and('include', 'per-page-25')
-          cy.get('@maxItems').click()
-        })
-    cy.get('tbody').find('tr').should('have.length', 50)
-      });
-    });
-    
+  });
+});
--- cypress/support/pagination.js
+++ cypress/support/pagination.js
@@ -24,7 +24,7 @@ Cypress.Commands.add('getPaginationArrows', (cyParent, childBtnAction, ...args) 
     throw `Unable to find "${btnAction}" button for data-cy parent: "${cyParent}"`
   })
 
-  /**
+/**
  * This command get a parent element using data-cy
  * then get's a child from it also using data-cy
  * 
@@ -49,3 +49,79 @@ Cypress.Commands.add('getItemsToggle', (cyParent, childBtnAction, ...args) => {
     throw `Unable to find "${btnAction}" button for data-cy parent: "${cyParent}"`
   })
 
+/**
+ * This command gets the pagination menu (top/bottom)
+ * and tests navigation arrows
+ * 
+ * Example usage:
+ * cy.testNavArrowsFlow("top_pagination")
+ * 
+ * @param {String} selector - The parent data-cy element
+ */
+Cypress.Commands.add('testNavArrowsFlow', (selector) => {
+  // TODO: navigate through ALL pages
+
+  cy.getPaginationBtn(`${selector}`, 'next').as('nextBtn')
+  cy.getPaginationBtn(`${selector}`, 'previous').as('previousBtn')
+  
+  cy.get('@previousBtn').should('be.disabled')
+  cy.get('@nextBtn').should('not.be.disabled').click()
+  
+  cy.getPaginationBtn(`${selector}`, 'next').as('nextBtn')
+  cy.getPaginationBtn(`${selector}`, 'previous').as('previousBtn')
+
+  cy.get('@nextBtn').should('not.be.disabled')
+  cy.get('@previousBtn').should('not.be.disabled').click()
+
+  });
+
+/**
+ * This command gets the pagination menu (top/bottom)
+ * and tests navigation arrows
+ * 
+ * Example usage:
+ * cy.testItemsList("top_pagination")
+ * 
+ * @param {String} selector - The parent data-cy element
+ */
+Cypress.Commands.add('testItemsListFlow', (selector) => {
+  // TODO: test all values of items per page
+
+  // get table and amount of lines
+  cy.get('tbody').find('tr').should('have.length', 10)
+  
+  // toggle the list
+  cy.getByCy(`${selector}`).find('.pf-c-options-menu').as('pag_option_menu');
+  cy.findByIdLike('@pag_option_menu', 'aa-pagination-toggle')
+  .should('have.attr', 'aria-expanded', 'false').click()
+  cy.findByIdLike('@pag_option_menu', 'aa-pagination-toggle')
+  .should('have.attr', 'aria-expanded', 'true')
+
+  // assert the options available
+  cy.get('@pag_option_menu')
+  .find('ul', 'per-page-5').should(($ul) => {
+      const n = parseFloat($ul.text())
+      expect(n).to.be.eq(5)
+    }).within(($ul) => {
+        cy.get('li').eq(4).find('button').as('maxItems')
+
+        cy.get('li').eq(0).find('button').should('have.attr', 'data-action').and('include', 'per-page-5')
+        cy.get('li').eq(1).find('button').should('have.attr', 'data-action').and('include', 'per-page-10')
+        cy.get('li').eq(2).find('button').should('have.attr', 'data-action').and('include', 'per-page-15')
+        cy.get('li').eq(3).find('button').should('have.attr', 'data-action').and('include', 'per-page-20')
+        cy.get('@maxItems').should('have.attr', 'data-action').and('include', 'per-page-25')
+        cy.get('@maxItems').click()
+      })
+  cy.get('tbody').find('tr').should('have.length', 50)
+  
+  // toggle back to 5 items
+  cy.findByIdLike('@pag_option_menu', 'aa-pagination-toggle')
+  .should('have.attr', 'aria-expanded', 'false').click()
+  cy.findByIdLike('@pag_option_menu', 'aa-pagination-toggle')
+  .should('have.attr', 'aria-expanded', 'true')
+
+  cy.get('@pag_option_menu').find('li').eq(0).as('5_items')
+  cy.get('@5_items').click()
+  cy.get('tbody').find('tr').should('have.length', 10)
+  });
+  
