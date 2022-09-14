f5d2efd353b7816256d8c51e1ab8ba3d3eea0626
Zita Nemeckova <znemecko@redhat.com>
2022-09-14 14:51:51 +0200

    Update items in update function

--- src/Containers/Reports/Layouts/AutomationCalculator/AutomationCalculator.tsx
+++ src/Containers/Reports/Layouts/AutomationCalculator/AutomationCalculator.tsx
@@ -178,9 +178,7 @@ const AutomationCalculator: FC<AutmationCalculatorProps> = ({
     const res = await readData(queryParams);
     api.result.monetary_gain_current_page = res.monetary_gain_current_page;
     api.result.monetary_gain_other_pages = res.monetary_gain_other_pages;
-    api.result.items = api.result.items.map((item, i) =>
-      Object.assign({}, item, res.meta.legend[i])
-    );
+    setValue(mapApi(res.meta));
     return res;
   };
 
@@ -250,15 +248,6 @@ const AutomationCalculator: FC<AutmationCalculatorProps> = ({
       return;
     }
     await update();
-    updatedData.forEach((item, i) => {
-      if (item.id === api.result.items[i].id) {
-        item.manual_effort_minutes = api.result.items[i].manual_effort_minutes;
-        item.successful_hosts_savings =
-          api.result.items[i].successful_hosts_savings;
-        item.monetary_gain = api.result.items[i].monetary_gain;
-      }
-    });
-    setValue(updatedData);
   };
 
   const setEnabled = (id) => async (value) => {
