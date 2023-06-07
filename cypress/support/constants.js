c8fa02b71be6d19f74b570a043afadfb4093eb8e
Ingrid Sena <10999758+fullsushidev@users.noreply.github.com>
2023-06-07 12:45:19 +0200

    Prettier linter

--- cypress/support/constants.js
+++ cypress/support/constants.js
@@ -4,44 +4,43 @@
 // the below change foreces this in the url
 // https://env-ephemeral-kehfbd-hwwsn2eu.apps.c-rh-c-eph.8p0c.p1.openshiftapps.com/ansible/automation-analytics/reports/aa_2_1_onboarding
 
-// local
-// ephemeral
-// stage
-
 export const ENVS = {
-	LOCAL: 0,
-	EPHEMERAL: 1,
-	STAGE: 2
-}
+  LOCAL: 0,
+  EPHEMERAL: 1,
+  STAGE: 2,
+};
 
-export const ENV = (Cypress.env('test_env')) == undefined ? ENVS.LOCAL : parseInt(Cypress.env('test_env'))
+export const ENV =
+  Cypress.env('test_env') == undefined
+    ? ENVS.LOCAL
+    : parseInt(Cypress.env('test_env'));
 
-export const appid = Cypress.env('appid')
-export const aapUrl = '/ansible/automation-analytics'
-export const dashboardUrl = '/ansible/automation-analytics'
-export const orgsUrl = '/organization-statistics'
-export const jobExplorerUrl = '/job-explorer'
-export const clustersUrl = '/clusters'
-export const reportsUrl = '/reports'
-export const savingsPlannerUrl = '/savings-planner'
-export const calculatorUrl = '/reports/automation_calculator'
-export const calculatorUrlDirect = '/automation_calculator'
-export const notificationsUrl = '/notifications'
+export const appid = Cypress.env('appid');
+export const aapUrl = '/ansible/automation-analytics';
+export const dashboardUrl = '/ansible/automation-analytics';
+export const orgsUrl = '/organization-statistics';
+export const jobExplorerUrl = '/job-explorer';
+export const clustersUrl = '/clusters';
+export const reportsUrl = '/reports';
+export const savingsPlannerUrl = '/savings-planner';
+export const calculatorUrl = '/reports/automation_calculator';
+export const calculatorUrlDirect = '/automation_calculator';
+export const notificationsUrl = '/notifications';
 
-export const hcbjt = 'hosts_changed_by_job_template'
-export const cmbjt = 'changes_made_by_job_template'
-export const jtrr = 'job_template_run_rate'
-export const hbo = 'hosts_by_organization'
-export const jtbo = 'jobs_and_tasks_by_organization'
-export const texp = 'templates_explorer'
-export const mum = 'most_used_modules'
-export const mubo = 'module_usage_by_organization'
-export const mubjt = 'module_usage_by_job_template'
-export const mubt = 'module_usage_by_task'
-export const aa21m = 'aa_2_1_onboarding'
-export const hab = 'host_anomalies_bar'
-export const has = 'host_anomalies_scatter'
-export const tbo = 'templates_by_organization'
+export const hcbjt = 'hosts_changed_by_job_template';
+export const cmbjt = 'changes_made_by_job_template';
+export const jtrr = 'job_template_run_rate';
+export const hbo = 'hosts_by_organization';
+export const jtbo = 'jobs_and_tasks_by_organization';
+export const texp = 'templates_explorer';
+export const mum = 'most_used_modules';
+export const mubo = 'module_usage_by_organization';
+export const mubjt = 'module_usage_by_job_template';
+export const mubt = 'module_usage_by_task';
+export const aa21m = 'aa_2_1_onboarding';
+export const hab = 'host_anomalies_bar';
+export const has = 'host_anomalies_scatter';
+export const tbo = 'templates_by_organization';
 
 export const allReports = [
   hcbjt,
@@ -57,12 +56,9 @@ export const allReports = [
   aa21m,
   hab,
   has,
-  tbo
-]
+  tbo,
+];
 
 export const skippedTests = {
-  "reports": [
-    "host_anomalies_scatter",
-    "templates_by_organization"
-  ]
-}
+  reports: ['host_anomalies_scatter', 'templates_by_organization'],
+};
