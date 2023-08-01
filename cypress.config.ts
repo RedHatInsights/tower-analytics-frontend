57c9f0f723b6f5036eb2f268dfa4885fe0872499
Ingrid Sena <10999758+fullsushidev@users.noreply.github.com>
2023-08-01 18:48:26 +0200

    Including baseUrl into config again
    As per cypress docs

--- cypress.config.ts
+++ cypress.config.ts
@@ -20,5 +20,6 @@ export default defineConfig({
   },
   e2e: {
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+    baseUrl: 'https://stage.foo.redhat.com:1337/ansible/automation-analytics',
   },
 });
