db8380571c6ac65648dce4b64f3cf7808a1b6a35
Ingrid Sena <10999758+fullsushidev@users.noreply.github.com>
2023-08-01 18:49:56 +0200

    Include new login strategy
    - cleans code
    - introduce sessions
    - prevents cypress to login via UI for all specs

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -29,15 +29,5 @@ Cypress.on('uncaught:exception', (_err, _runnable) => {
 
 // these cokies should resolve the accept cookies dialog
 beforeEach(() => {
-  Cypress.Cookies.debug(true);
-  cy.setCookie('cmapi_cookie_privacy', 'permit 1,2,3', { secure: true });
-  cy.setCookie('cmapi_gtm_bl', '', { secure: true });
-  cy.setCookie('notice_preferences', '2:', { secure: true });
-  cy.setCookie('notice_behavior', 'expressed,eu', { secure: true });
-  cy.setCookie('notice_gdpr_prefs', '0,1,2:', {
-    secure: true,
-    domain: 'redhat.com',
-  });
-
-  cy.login();
+  cy.loginWithPageSession();
 });
