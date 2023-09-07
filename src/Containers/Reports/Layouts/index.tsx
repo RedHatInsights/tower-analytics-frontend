1f79683e00c6b53d09ae48dc5a167e57927f0653
Maeve Hoffer <dhoffer@redhat.com>
2023-09-07 10:40:12 -0700

    edit getLayoutComponenent to take report as string

--- src/Containers/Reports/Layouts/index.tsx
+++ src/Containers/Reports/Layouts/index.tsx
@@ -4,22 +4,28 @@ import AutomationCalculator from './AutomationCalculator';
 import { ReportSchema } from './types';
 
 const getLayoutComponent = (
-  report: ReportSchema,
+  report: ReportSchema | string,
   fullCard: boolean
 ): React.ReactElement => {
-  switch (report.layoutComponent) {
-    case 'standard':
-      if (fullCard === true) return <StandardReport {...report.layoutProps} />;
-      else return <StandardReport {...report.layoutProps} fullCard={false} />;
-    case 'automationCalculator':
-      if (fullCard === true)
-        return <AutomationCalculator {...report.layoutProps} />;
-      else
-        return (
-          <AutomationCalculator {...report.layoutProps} fullCard={false} />
-        );
-    default:
-      return <></>;
+  if (typeof report === 'string') {
+    console.log('string');
+    return <></>;
+  } else {
+    switch (report.layoutComponent) {
+      case 'standard':
+        if (fullCard === true)
+          return <StandardReport {...report.layoutProps} />;
+        else return <StandardReport {...report.layoutProps} fullCard={false} />;
+      case 'automationCalculator':
+        if (fullCard === true)
+          return <AutomationCalculator {...report.layoutProps} />;
+        else
+          return (
+            <AutomationCalculator {...report.layoutProps} fullCard={false} />
+          );
+      default:
+        return <></>;
+    }
   }
 };
 
