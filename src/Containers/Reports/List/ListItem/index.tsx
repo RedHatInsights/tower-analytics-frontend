f9509228ca59e71307e639ca0473682bd3ffb2b8
Maeve Hoffer <dhoffer@redhat.com>
2023-10-03 13:02:26 -0700

    remove navigate from listitem

--- src/Containers/Reports/List/ListItem/index.tsx
+++ src/Containers/Reports/List/ListItem/index.tsx
@@ -1,5 +1,5 @@
 import React, { FunctionComponent } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 import {
