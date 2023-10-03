f62847cd3386f1ad4e8410e817ed1a416deef7d3
Maeve Hoffer <dhoffer@redhat.com>
2023-10-03 13:04:08 -0700

    remove extra console.logs

--- src/Containers/Reports/Layouts/index.tsx
+++ src/Containers/Reports/Layouts/index.tsx
@@ -7,14 +7,11 @@ const getLayoutComponent = (
   report: ReportSchema,
   fullCard: boolean
 ): React.ReactElement => {
-  //console.log('calling getLayoutComponent');
   switch (report.layoutComponent) {
     case 'standard':
-      //console.log('getLayoutComponent, in switch case "standard"');
       if (fullCard === true) return <StandardReport {...report.layoutProps} />;
       else return <StandardReport {...report.layoutProps} fullCard={false} />;
     case 'automationCalculator':
-      //console.log('getLayoutComponent, in switch case "automation calculator"');
       if (fullCard === true)
         return <AutomationCalculator {...report.layoutProps} />;
       else
@@ -22,7 +19,6 @@ const getLayoutComponent = (
           <AutomationCalculator {...report.layoutProps} fullCard={false} />
         );
     default:
-      //console.log('getLayoutComponent, in switch case "default"');
       return <></>;
   }
 };
