7dc356f35312fd26e1ed9f4a5a551e253db17c90
Pratyush Bhandari <prbhanda@redhat.com>
2024-10-31 10:57:37 -0400

    Create a proxy for local chrome

--- fec.config.js
+++ fec.config.js
@@ -5,7 +5,9 @@ module.exports = {
   appUrl: ['/ansible/automation-analytics/', '/ansible/automation-analytics'],
   plugins: [],
   // sassPrefix: '.automation-analytics, .automationAnalytics',
-
+  routes: {
+    '/api/chrome-service/v1/static': { host: 'http://127.0.0.1:8000' },
+  },
   moduleFederation: {
     exclude: ['react-router-dom'],
     shared: [
